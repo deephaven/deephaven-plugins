d7fb53c330b5a7697f524d0ec37178fbbfed85a0
Devin Smith <devinsmith@deephaven.io>
2021-12-31 10:13:51 -0800

    Implement FigureType as class

--- src/deephaven/plugin/matplotlib/__init__.py
+++ src/deephaven/plugin/matplotlib/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.0.1.dev1"
 
 def register_into(callback):
-    register_custom_types_into(callback)
+    register_object_types_into(callback)
 
-def register_custom_types_into(callback):
+def register_object_types_into(callback):
     from . import figure_type
-    callback.register_custom_type(figure_type)
+    callback.register_object_type(figure_type.FigureType())
--- src/deephaven/plugin/matplotlib/figure_type.py
+++ src/deephaven/plugin/matplotlib/figure_type.py
@@ -3,13 +3,15 @@ from matplotlib.figure import Figure
 
 NAME = "matplotlib.figure.Figure"
 
-def name() -> str:
-    return NAME
+class FigureType:
+    @property
+    def name(self) -> str:
+        return NAME
 
-def is_type(object) -> bool:
-    return isinstance(object, Figure)
+    def is_type(self, object) -> bool:
+        return isinstance(object, Figure)
 
-def to_bytes(exporter, figure: Figure) -> bytes:
-    buf = BytesIO()
-    figure.savefig(buf, format='PNG')
-    return buf.getvalue()
+    def to_bytes(self, exporter, figure: Figure) -> bytes:
+        buf = BytesIO()
+        figure.savefig(buf, format='PNG')
+        return buf.getvalue()
