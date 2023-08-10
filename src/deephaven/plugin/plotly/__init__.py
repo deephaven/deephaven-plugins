a626a9dd005fcda7218366c9267a500474dc9313
Colin Alworth <colin@vertispan.com>
2023-08-10 15:23:28 -0500

    Update deephaven-plugin to latest, fixed API breaking changes (#8)
    Also fixed minor style issues

--- src/deephaven/plugin/plotly/__init__.py
+++ src/deephaven/plugin/plotly/__init__.py
@@ -1,7 +1,8 @@
-from deephaven.plugin import Registration
+from deephaven.plugin import Registration, Callback
 from plotly import io as pio
 
 __version__ = "0.1.0"
+
 
 def _init_theme():
     # Set the Deephaven style globally
@@ -10,9 +11,10 @@ def _init_theme():
     # Disable default renderer to ignore figure.show()
     pio.renderers.default = None
 
+
 class PlotlyRegistration(Registration):
     @classmethod
-    def register_into(cls, callback: Registration.Callback) -> None:
+    def register_into(cls, callback: Callback) -> None:
         _init_theme()
         from . import figure_type
         callback.register(figure_type.FigureType)
--- src/deephaven/plugin/plotly/figure_type.py
+++ src/deephaven/plugin/plotly/figure_type.py
@@ -1,20 +1,21 @@
 from plotly.graph_objects import Figure
-from deephaven.plugin.object import Exporter, ObjectType
-import json
+from deephaven.plugin.object_type import Exporter, FetchOnlyObjectType
 
 # Name of the plotly figure object that was exported
 NAME = "plotly.figure"
 
+
 def _export_figure(figure):
     return figure.to_json().encode()
 
-class FigureType(ObjectType):
+
+class FigureType(FetchOnlyObjectType):
     @property
     def name(self) -> str:
         return NAME
 
-    def is_type(self, object) -> bool:
-        return isinstance(object, Figure)
+    def is_type(self, obj) -> bool:
+        return isinstance(obj, Figure)
 
     def to_bytes(self, exporter: Exporter, figure: Figure) -> bytes:
         return _export_figure(figure)
