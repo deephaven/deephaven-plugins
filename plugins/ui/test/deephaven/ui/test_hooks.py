b15a04635594c295a13cf0c60a6b8cc7bcc323f4
mikebender <mikebender@deephaven.io>
2023-09-13 15:41:05 -0400

    Add tests for use_memo

--- plugins/ui/test/deephaven/ui/test_hooks.py
+++ plugins/ui/test/deephaven/ui/test_hooks.py
@@ -98,6 +98,42 @@ class HooksTest(BaseTestCase):
         result = rerender(3)
         self.assertEqual(result.current, 2)
 
+    def test_memo(self):
+        from deephaven.ui.hooks import use_memo
+
+        def _test_memo(fn=lambda: "foo", a=1, b=2):
+            return use_memo(fn, [a, b])
+
+        # Initial render
+        render_result = render_hook(_test_memo)
+        result, rerender = itemgetter("result", "rerender")(render_result)
+        self.assertEqual(result, "foo")
+
+        # Rerender with new function but same deps
+        # Should not re-run the function
+        mock = Mock(return_value="bar")
+        result = rerender(mock)
+        self.assertEqual(result, "foo")
+        self.assertEqual(mock.call_count, 0)
+
+        # Rerender with new deps
+        # Should re-run the function
+        result = rerender(mock, 3, 4)
+        self.assertEqual(result, "bar")
+        self.assertEqual(mock.call_count, 1)
+
+        # Rerender with the same new deps
+        # Should not re-run the function
+        result = rerender(mock, 3, 4)
+        self.assertEqual(result, "bar")
+        self.assertEqual(mock.call_count, 1)
+
+        # Rerender with new deps and new function
+        mock = Mock(return_value="biz")
+        result = rerender(mock, b=4)
+        self.assertEqual(result, "biz")
+        self.assertEqual(mock.call_count, 1)
+
 
 if __name__ == "__main__":
     unittest.main()
