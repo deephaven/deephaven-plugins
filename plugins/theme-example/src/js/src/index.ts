03df65801a6c6cfc6082537f8d29b03030125c46
Brian Ingles <brianingles@deephaven.io>
2023-09-20 13:00:27 -0500

    Sample ThemePlugins

---
+++ plugins/theme-example/src/js/src/index.ts
@@ -0,0 +1,43 @@
+import acmeDark from './theme_dark.scss?inline';
+import acmeLight from './theme_light.scss?inline';
+import acmeCool from './theme_cool.scss?inline';
+
+/** TODO: Define these types in @deephaven/plugin */
+
+interface ThemePluginConfig {
+  name: string;
+  baseTheme: 'dark' | 'light';
+  styleContent: string;
+}
+
+interface ThemePlugin {
+  name: string;
+  type: 'ThemePlugin';
+  config: ThemePluginConfig | ThemePluginConfig[];
+}
+
+/** Plugin */
+
+const plugin: ThemePlugin = {
+  name: 'Acme Theme Plugin',
+  type: 'ThemePlugin',
+  config: [
+    {
+      name: 'Acme Dark',
+      baseTheme: 'dark',
+      styleContent: acmeDark,
+    },
+    {
+      name: 'Acme Light',
+      baseTheme: 'light',
+      styleContent: acmeLight,
+    },
+    {
+      name: 'Acme Cool',
+      baseTheme: 'light',
+      styleContent: acmeCool,
+    },
+  ],
+};
+
+export default plugin;
