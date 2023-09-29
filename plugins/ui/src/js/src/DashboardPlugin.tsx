263b3fc5cb8a14de5b5aab8767b14213e6726aca
mikebender <mikebender@deephaven.io>
2023-09-29 10:05:58 -0400

    WIP Use the registration rather than useDashboardPanel
    - Need to move the element decoding logic into DashboardPlugin instead so it can open panels when necessary

--- plugins/ui/src/js/src/DashboardPlugin.tsx
+++ plugins/ui/src/js/src/DashboardPlugin.tsx
@@ -1,22 +1,79 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import shortid from 'shortid';
 import {
   DashboardPluginComponentProps,
-  useDashboardPanel,
+  LayoutUtils,
+  useListener,
 } from '@deephaven/dashboard';
+import Log from '@deephaven/log';
+import type { VariableDefinition } from '@deephaven/jsapi-types';
 import ElementPanel from './ElementPanel';
 import styles from './styles.scss?inline';
 
 const NAME_ELEMENT = 'deephaven.ui.Element';
 
-export function DashboardPlugin(
-  props: DashboardPluginComponentProps
-): JSX.Element | null {
-  useDashboardPanel({
-    dashboardProps: props,
-    component: ElementPanel,
-    componentName: ElementPanel.displayName,
-    supportedTypes: [NAME_ELEMENT],
-  });
+const log = Log.module('@deephaven/js-plugin-ui.DashboardPlugin');
+
+export function DashboardPlugin({
+  id,
+  layout,
+  registerComponent,
+}: DashboardPluginComponentProps): JSX.Element | null {
+  const handlePanelOpen = useCallback(
+    ({
+      dragEvent,
+      fetch,
+      metadata = {},
+      panelId = shortid.generate(),
+      widget,
+    }: {
+      dragEvent?: React.DragEvent;
+      fetch: () => Promise<unknown>;
+      metadata?: Record<string, unknown>;
+      panelId?: string;
+      widget: VariableDefinition;
+    }) => {
+      const { id: widgetId, name, type } = widget;
+      if ((type as string) !== NAME_ELEMENT) {
+        // Only want to listen for matplotlib panels
+        return;
+      }
+      log.info('Panel opened of type', type);
+      const config = {
+        type: 'react-component' as const,
+        component: ElementPanel.displayName,
+        props: {
+          localDashboardId: id,
+          id: panelId,
+          metadata: {
+            ...metadata,
+            id: widgetId,
+            name,
+            type,
+          },
+          fetch,
+        },
+        title: name,
+        id: panelId,
+      };
+
+      const { root } = layout;
+      LayoutUtils.openComponent({ root, config, dragEvent });
+    },
+    [id, layout]
+  );
+
+  useEffect(() => {
+    const cleanups = [
+      registerComponent(ElementPanel.displayName, ElementPanel),
+    ];
+
+    return () => {
+      cleanups.forEach(cleanup => cleanup());
+    };
+  }, [registerComponent]);
+
+  useListener(layout.eventHub, 'PanelEvent.OPEN', handlePanelOpen);
 
   return <style>{styles}</style>;
 }
