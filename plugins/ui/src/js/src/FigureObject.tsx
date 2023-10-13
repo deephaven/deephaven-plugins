3de55f563c741a4646ee370972749a37dcbe3aff
mikebender <mikebender@deephaven.io>
2023-10-13 11:56:05 -0400

    Add resize observer to FigureObject as well
    - Now it resizes correctly as it is moved around

--- plugins/ui/src/js/src/FigureObject.tsx
+++ plugins/ui/src/js/src/FigureObject.tsx
@@ -1,34 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Chart, ChartModel, ChartModelFactory } from '@deephaven/chart';
 import { useApi } from '@deephaven/jsapi-bootstrap';
 import type { Figure } from '@deephaven/jsapi-types';
-import shortid from 'shortid';
+import Log from '@deephaven/log';
+import { View } from '@adobe/react-spectrum';
+import {
+  ReactSpectrumComponent,
+  extractSpectrumHTMLElement,
+} from '@deephaven/react-hooks';
+import useResizeObserver from './useResizeObserver';
+
+const log = Log.module('@deephaven/js-plugin-ui/FigureObject');
 
 export interface FigureObjectProps {
   object: Figure;
 }
 
-function FigureObject(props: FigureObjectProps) {
-  const { object } = props;
+function FigureObject({ object }: FigureObjectProps) {
   const dh = useApi();
   const [model, setModel] = useState<ChartModel>();
-  const [key, setKey] = useState(shortid());
+  const chart = useRef<Chart>(null);
+  const viewComponent = useRef<ReactSpectrumComponent>(null);
+  const viewElement = extractSpectrumHTMLElement(viewComponent.current);
 
   useEffect(() => {
     async function loadModel() {
       const newModel = await ChartModelFactory.makeModel(dh, undefined, object);
       setModel(newModel);
-
-      // TODO: Chart.tsx doesn't handle the case where the model has been updated. Update the key so we get a new chart every time the model updates.
-      setKey(shortid());
     }
     loadModel();
   }, [dh, object]);
 
+  const handleResize = useCallback(
+    (entries: ResizeObserverEntry[]) => {
+      log.debug2('handleResize', entries);
+      chart.current?.updateDimensions();
+    },
+    [chart]
+  );
+
+  useResizeObserver(viewElement, handleResize);
+
   return (
-    <div className="ui-figure-object">
-      {model && <Chart model={model} key={key} />}
-    </div>
+    <View
+      width="100%"
+      height="100%"
+      overflow="hidden"
+      position="relative"
+      ref={viewComponent}
+    >
+      {model && <Chart model={model} ref={chart} />}
+    </View>
   );
 }
 
