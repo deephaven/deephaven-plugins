c29f6aeffa5f9d139befb9fe550f1739bd1c9229
mikebender <mikebender@deephaven.io>
2023-10-26 11:47:21 -0400

    Add unit tests for WidgetHandler

--- plugins/ui/src/js/src/WidgetHandler.test.tsx
+++ plugins/ui/src/js/src/WidgetHandler.test.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { WidgetDefinition } from '@deephaven/dashboard';
 import { TestUtils } from '@deephaven/utils';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import WidgetHandler, { WidgetHandlerProps } from './WidgetHandler';
 import { JsWidget, WidgetWrapper } from './WidgetTypes';
+import { DocumentHandlerProps } from './DocumentHandler';
 
+const mockApi = { Widget: { EVENT_MESSAGE: 'message' } };
 jest.mock('@deephaven/jsapi-bootstrap', () => ({
-  useApi: jest.fn(() => ({
-    Widget: { EVENT_MESSAGE: 'message' },
-  })),
+  useApi: jest.fn(() => mockApi),
 }));
+
+const mockDocumentHandler = jest.fn((props: DocumentHandlerProps) => (
+  <div>Document Handler</div>
+));
 jest.mock(
   './DocumentHandler',
-  () =>
-    function mockDocumentHandler() {
-      return <div>Document Handler</div>;
-    }
+  () => (props: DocumentHandlerProps) => mockDocumentHandler(props)
 );
+
+function makeDocumentUpdatedJsonRpc(document: Record<string, unknown> = {}) {
+  return {
+    jsonrpc: '2.0',
+    method: 'documentUpdated',
+    params: [document],
+  };
+}
+
+function makeDocumentUpdatedJsonRpcString(
+  document: Record<string, unknown> = {}
+) {
+  return JSON.stringify(makeDocumentUpdatedJsonRpc(document));
+}
 
 function makeWidgetDefinition({
   id = 'widget-id',
@@ -30,16 +45,19 @@ function makeWidgetDefinition({
   };
 }
 
+function makeWidget({
+  addEventListener = jest.fn(() => jest.fn()),
+  getDataAsString = () => makeDocumentUpdatedJsonRpcString(),
+}: Partial<JsWidget> = {}): JsWidget {
+  return TestUtils.createMockProxy<JsWidget>({
+    addEventListener,
+    getDataAsString,
+  });
+}
+
 function makeWidgetWrapper({
   definition = makeWidgetDefinition(),
-  fetch = () =>
-    Promise.resolve(
-      TestUtils.createMockProxy<JsWidget>({
-        addEventListener: jest.fn(() => jest.fn()),
-        getDataAsString: () =>
-          '{"jsonrpc":"2.0","method":"documentUpdated","params":[{}]}',
-      })
-    ),
+  fetch = () => Promise.resolve(makeWidget()),
 }: Partial<WidgetWrapper> = {}): WidgetWrapper {
   return {
     id: definition.id ?? 'widget-id',
@@ -55,7 +73,72 @@ function makeWidgetHandler({
   return <WidgetHandler widget={widget} onClose={onClose} />;
 }
 
+beforeEach(() => {
+  mockDocumentHandler.mockClear();
+});
+
 it('mounts and unmounts', async () => {
   const { unmount } = render(makeWidgetHandler());
   unmount();
+});
+
+it('updates the document when event is received', async () => {
+  let fetchResolve: (value: JsWidget | PromiseLike<JsWidget>) => void;
+  const fetchPromise = new Promise<JsWidget>(resolve => {
+    fetchResolve = resolve;
+  });
+  const fetch = jest.fn(() => fetchPromise);
+  const definition = makeWidgetDefinition();
+  const cleanup = jest.fn();
+  const mockAddEventListener = jest.fn(() => cleanup);
+  const initialDocument = { foo: 'bar' };
+  const widget = makeWidget({
+    addEventListener: mockAddEventListener,
+    getDataAsString: jest.fn(() =>
+      makeDocumentUpdatedJsonRpcString(initialDocument)
+    ),
+  });
+  const wrapper = makeWidgetWrapper({ definition, fetch });
+  const { unmount } = render(makeWidgetHandler({ widget: wrapper }));
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(mockAddEventListener).not.toHaveBeenCalled();
+  expect(mockDocumentHandler).not.toHaveBeenCalled();
+  await act(async () => {
+    fetchResolve!(widget);
+    await fetchPromise;
+  });
+
+  expect(mockAddEventListener).toHaveBeenCalledTimes(1);
+  expect(mockDocumentHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      definition,
+      element: initialDocument,
+    })
+  );
+
+  mockDocumentHandler.mockClear();
+
+  const updatedDocument = { fiz: 'baz' };
+
+  act(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (mockAddEventListener.mock.calls[0] as any)[1]({
+      detail: {
+        getDataAsString: jest.fn(() =>
+          makeDocumentUpdatedJsonRpcString(updatedDocument)
+        ),
+        exportedObjects: [],
+      },
+    });
+  });
+  expect(mockDocumentHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      definition,
+      element: updatedDocument,
+    })
+  );
+
+  expect(cleanup).not.toHaveBeenCalled();
+  unmount();
+  expect(cleanup).toHaveBeenCalledTimes(1);
 });
