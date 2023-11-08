39561ad633ec0c0a5258c643c6c9d1262dbb0007
mikebender <mikebender@deephaven.io>
2023-11-08 12:49:55 -0500

    Use the chart theme

--- plugins/ui/src/js/src/FigureObject.tsx
+++ plugins/ui/src/js/src/FigureObject.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Chart, ChartModel, ChartModelFactory } from '@deephaven/chart';
+import {
+  Chart,
+  ChartModel,
+  ChartModelFactory,
+  useChartTheme,
+} from '@deephaven/chart';
 import { useApi } from '@deephaven/jsapi-bootstrap';
 import type { Figure } from '@deephaven/jsapi-types';
 import { View } from '@adobe/react-spectrum';
@@ -11,14 +16,20 @@ export interface FigureObjectProps {
 function FigureObject({ object }: FigureObjectProps) {
   const dh = useApi();
   const [model, setModel] = useState<ChartModel>();
+  const chartTheme = useChartTheme();
 
   useEffect(() => {
     async function loadModel() {
-      const newModel = await ChartModelFactory.makeModel(dh, undefined, object);
+      const newModel = await ChartModelFactory.makeModel(
+        dh,
+        undefined,
+        object,
+        chartTheme
+      );
       setModel(newModel);
     }
     loadModel();
-  }, [dh, object]);
+  }, [chartTheme, dh, object]);
 
   return (
     <View flexGrow={1} flexShrink={1} overflow="hidden" position="relative">
