bbb0a65277761ae9d591aacc0cfbf382b7d8d811
Joe <josephnumainville@deephaven.io>
2023-11-30 09:40:37 -0600

    feat: add use_table_listener hook (#115)
    Fixes #96 
    
    Adds a hook that uses a table listener
    Here's an example that will monitor added or removed rows in the most
    recent update, depending on if the appropriate checkboxes are checked
    
    ```
    import deephaven.ui as ui
    from deephaven.table import Table
    from deephaven import time_table, empty_table, merge
    from deephaven.table_listener import listen
    from deephaven import pandas as dhpd
    import pandas as pd
    
    def to_table(update):
        return dhpd.to_table(pd.DataFrame.from_dict(update)) 
    
    def add_as_op(ls, t, op):
        t = t.update(f"type=`{op}`")
        ls.append(t)
    
    @ui.component
    def monitor_changed_data(source: Table):
    
        changed, set_changed = ui.use_state(empty_table(0))
    
        show_added, set_show_added = ui.use_state(True)
        show_removed, set_show_removed = ui.use_state(True)
    
        def listener(update, is_replay):
    
            to_merge = []
    
            if (added_dict := update.added()) and show_added:
                added = to_table(added_dict)
                add_as_op(to_merge, added, "added")
    
            if (removed_dict := update.removed()) and show_removed:
                removed = to_table(removed_dict)
                add_as_op(to_merge, removed, "removed")
    
            if to_merge:
                set_changed(merge(to_merge))
            else:
                set_changed(empty_table(0))
    
        ui.use_table_listener(source, listener)
    
        added_check = ui.checkbox("Show Added", isSelected=show_added, on_change=set_show_added)
    
        removed_check = ui.checkbox("Show Removed", isSelected=show_removed, on_change=set_show_removed)
    
        return [added_check, removed_check, changed]
    
    t = time_table("PT1S").update(formulas=["X=i"]).tail(5)
    
    monitor = monitor_changed_data(t)
    ```

---
+++ plugins/ui/src/deephaven/ui/hooks/use_table_listener.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from functools import partial
+from typing import Callable, Literal
+
+from deephaven.table import Table
+from deephaven.table_listener import listen, TableUpdate, TableListener
+from deephaven.execution_context import get_exec_ctx, ExecutionContext
+
+from .use_effect import use_effect
+
+LockType: Literal["shared", "exclusive"]
+
+
+def listener_with_ctx(
+    exec_ctx: ExecutionContext,
+    listener: Callable[[TableUpdate, bool], None],
+    update: TableUpdate,
+    is_replay: bool,
+) -> None:
+    """
+    Call the listener within an execution context.
+
+    Args:
+        exec_ctx: ExecutionContext: The execution context to use.
+        listener: Callable[[TableUpdate, bool], None]: The listener to call.
+        update: TableUpdate: The update to pass to the listener.
+        is_replay: bool: Whether the update is a replay.
+    """
+    with exec_ctx:
+        listener(update, is_replay)
+
+
+def with_ctx(
+    listener: Callable[[TableUpdate, bool], None]
+) -> partial[[TableUpdate, bool], None]:
+    """
+    Wrap the listener in an execution context.
+
+    Args:
+        listener: The listener to wrap.
+
+    Returns:
+        partial[[TableUpdate, bool], None]: The wrapped listener.
+    """
+    return partial(listener_with_ctx, get_exec_ctx(), listener)
+
+
+def wrap_listener(
+    listener: Callable[[TableUpdate, bool], None] | TableListener
+) -> partial[[TableUpdate, bool], None]:
+    """
+    Wrap the listener in an execution context.
+
+    Args:
+        listener: Callable[[TableUpdate, bool], None]: The listener to wrap.
+
+    Returns:
+        partial[[TableUpdate, bool], None]: The wrapped listener.
+    """
+    if isinstance(listener, TableListener):
+        return with_ctx(listener.on_update)
+    elif callable(listener):
+        return with_ctx(listener)
+
+
+def use_table_listener(
+    table: Table,
+    listener: Callable[[TableUpdate, bool], None] | TableListener,
+    description: str | None = None,
+    do_replay: bool = False,
+    replay_lock: LockType = "shared",
+) -> None:
+    """
+    Listen to a table and call a listener when the table updates.
+
+    Args:
+        table: Table: The table to listen to.
+        listener: Callable[[TableUpdate, bool], None] | TableListener: Either a function or a TableListener with an
+        on_update function. The function must take a TableUpdate and is_replay bool.
+        description: str | None: An optional description for the UpdatePerformanceTracker to append to the listener’s
+        entry description, default is None.
+        do_replay: bool: Whether to replay the initial snapshot of the table, default is False.
+        replay_lock: LockType: The lock type used during replay, default is ‘shared’, can also be ‘exclusive’.
+    """
+
+    def start_listener() -> Callable[[], None]:
+        """
+        Start the listener. Returns a function that can be called to stop the listener by the use_effect hook.
+
+        Returns:
+            Callable[[], None]: A function that can be called to stop the listener by the use_effect hook.
+        """
+        handle = listen(
+            table,
+            wrap_listener(listener),
+            description=description,
+            do_replay=do_replay,
+            replay_lock=replay_lock,
+        )
+
+        return lambda: handle.stop()
+
+    use_effect(start_listener, [table, listener, description, do_replay, replay_lock])
