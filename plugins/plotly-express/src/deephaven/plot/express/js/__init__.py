2f58734b12834d407e2b0bca9decada99184e13f
Devin Smith <devinsmith@deephaven.io>
2023-12-13 11:27:23 -0800

    Update for deephaven-plugin==0.6.0

--- plugins/plotly-express/src/deephaven/plot/express/js/__init__.py
+++ plugins/plotly-express/src/deephaven/plot/express/js/__init__.py
@@ -1,9 +1,6 @@
-import typing
-import pathlib
-
-import importlib.metadata
 import importlib.resources
 import json
+import pathlib
 import sys
 from typing import Callable, ContextManager
 
@@ -16,12 +13,12 @@ class ExpressJsPlugin(JsPlugin):
         name: str,
         version: str,
         main: str,
-        root_provider: Callable[[], ContextManager[pathlib.Path]],
+        path: pathlib.Path,
     ) -> None:
         self._name = name
         self._version = version
         self._main = main
-        self._root_provider = root_provider
+        self._path = path
 
     @property
     def name(self) -> str:
@@ -35,26 +32,30 @@ class ExpressJsPlugin(JsPlugin):
     def main(self) -> str:
         return self._main
 
-    def distribution_path(self) -> ContextManager[pathlib.Path]:
-        # TODO: Finalize JsPlugin
-        # https://github.com/deephaven/deephaven-plugin/issues/15
-        return self._root_provider()
+    def path(self) -> pathlib.Path:
+        return self._path
 
 
 def _create_from_npm_package_json(
-    root_provider: Callable[[], ContextManager[pathlib.Path]]
+    path_provider: Callable[[], ContextManager[pathlib.Path]]
 ) -> JsPlugin:
-    with root_provider() as root, (root / "package.json").open("rb") as f:
+    with path_provider() as tmp_js_path:
+        js_path = tmp_js_path
+    if not js_path.exists():
+        raise Exception(
+            f"Package is not installed in a normal python filesystem, '{js_path}' does not exist"
+        )
+    with (js_path / "package.json").open("rb") as f:
         package_json = json.load(f)
     return ExpressJsPlugin(
         package_json["name"],
         package_json["version"],
         package_json["main"],
-        root_provider,
+        js_path,
     )
 
 
-def _production_root() -> ContextManager[pathlib.Path]:
+def _resource_js_path() -> ContextManager[pathlib.Path]:
     # TODO: Js content should be in same package directory
     # https://github.com/deephaven/deephaven-plugins/issues/139
     if sys.version_info < (3, 9):
@@ -65,12 +66,8 @@ def _production_root() -> ContextManager[pathlib.Path]:
         )
 
 
-def _development_root() -> ContextManager[pathlib.Path]:
-    raise NotImplementedError("TODO")
-
-
 def create_js_plugin() -> JsPlugin:
     # TODO: Include developer instructions for installing in editable mode
     # https://github.com/deephaven/deephaven-plugins/issues/93
     # TBD what editable mode looks like for JsPlugin
-    return _create_from_npm_package_json(_production_root)
+    return _create_from_npm_package_json(_resource_js_path)
