12d4c2cf614ed2e5e3ea605d0916b69be17e0134
Mike Bender <mikebender@deephaven.io>
2023-12-23 22:26:15 -0500

    Remove useless MatplotlibPanel

--- plugins/matplotlib/src/js/src/MatplotlibPlugin.ts
+++ plugins/matplotlib/src/js/src/MatplotlibPlugin.ts
@@ -1,14 +1,12 @@
 import { type WidgetPlugin, PluginType } from '@deephaven/plugin';
 import { vsGraph } from '@deephaven/icons';
 import { MatplotlibView } from './MatplotlibView';
-import { MatplotlibPanel } from './MatplotlibPanel';
 
 export const MatplotlibPlugin: WidgetPlugin = {
   name: '@deephaven/js-plugin-matplotlib',
   type: PluginType.WIDGET_PLUGIN,
   supportedTypes: 'matplotlib.figure.Figure',
   component: MatplotlibView,
-  panelComponent: MatplotlibPanel,
   icon: vsGraph,
 };
 
--- plugins/matplotlib/src/js/src/index.ts
+++ plugins/matplotlib/src/js/src/index.ts
@@ -1,6 +1,5 @@
 import { MatplotlibPlugin } from './MatplotlibPlugin';
 
-export * from './MatplotlibPanel';
 export * from './MatplotlibView';
 
 export default MatplotlibPlugin;
