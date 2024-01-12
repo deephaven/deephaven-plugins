7212976c1b5072558194794281660974e5075e9e
Joe Numainville <josephnumainville@deephaven.io>
2024-01-12 09:43:23 -0600

    fixing hook

--- plugins/ui/src/deephaven/ui/hooks/use_execution_context.py
+++ plugins/ui/src/deephaven/ui/hooks/use_execution_context.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from functools import partial
 from typing import Callable
 
 from deephaven.execution_context import get_exec_ctx, ExecutionContext
@@ -7,16 +8,29 @@ from deephaven.execution_context import get_exec_ctx, ExecutionContext
 from .use_ref import use_ref
 
 
-def use_execution_context(func: Callable, exec_ctx: ExecutionContext = None) -> None:
+def func_with_ctx(
+    exec_ctx: ExecutionContext,
+    func: Callable,
+) -> None:
     """
-    Execute a function within an execution context.
+    Call the function within an execution context.
 
     Args:
-        func: Callable: The function to execute.
+        exec_ctx: ExecutionContext: The execution context to use.
+        func: Callable: The function to call.
+    """
+    with exec_ctx:
+        func()
+
+
+def use_execution_context(exec_ctx: ExecutionContext = None) -> None:
+    """
+    Create an execution context wrapper for a function.
+
+    Args:
         exec_ctx: ExecutionContext: The execution context to use. Defaults to
             the current execution context if not provided.
     """
     exec_ctx = use_ref(exec_ctx if exec_ctx else get_exec_ctx())
 
-    with exec_ctx.current:
-        func()
+    return partial(func_with_ctx, exec_ctx.current)
--- plugins/ui/test/deephaven/ui/test_hooks.py
+++ plugins/ui/test/deephaven/ui/test_hooks.py
@@ -491,52 +491,52 @@ class HooksTest(BaseTestCase):
         self.assertEqual(result, expected)
 
     def test_execution_context(self):
-        from deephaven.ui.hooks import use_execution_context
+        from deephaven.ui.hooks import use_execution_context, use_state, use_memo
+        from deephaven import empty_table
 
-        called = False
+        def _test_execution_context():
+            with_exec_ctx = use_execution_context()
 
-        def execution_context_callback():
-            nonlocal called
-            called = True
+            def table_func():
+                # This would fail if not using an execution context
+                empty_table(0).update("X=1")
 
-        def _test_execution_context(exec_ctx=None):
-            use_execution_context(execution_context_callback, exec_ctx=exec_ctx)
+            def thread_func():
+                with_exec_ctx(table_func)
+
+            def start_thread():
+                thread = threading.Thread(target=thread_func)
+                thread.start()
+                thread.join()
+
+            use_memo(start_thread, [])
 
         render_hook(_test_execution_context)
 
-        self.assertTrue(called)
-
-    def test_execution_context_passed(self):
-        from deephaven.ui.hooks import use_execution_context
+    def test_execution_context_custom(self):
+        from deephaven.ui.hooks import use_execution_context, use_state, use_memo
+        from deephaven import empty_table
         from deephaven.execution_context import make_user_exec_ctx
 
-        called = False
-
-        def execution_context_callback():
-            nonlocal called
-            called = True
+        table = None
 
         def _test_execution_context():
-            use_execution_context(
-                execution_context_callback, exec_ctx=make_user_exec_ctx()
-            )
+            with_exec_ctx = use_execution_context(make_user_exec_ctx())
+
+            def table_func():
+                # This would fail if not using an execution context
+                empty_table(0).update("X=1")
+
+            def thread_func():
+                with_exec_ctx(table_func)
+
+            def start_thread():
+                thread = threading.Thread(target=thread_func)
+                thread.start()
+
+            use_memo(start_thread, [])
 
         render_hook(_test_execution_context)
-
-        self.assertTrue(called)
-
-    def test_execution_context_error(self):
-        from deephaven.ui.hooks import use_execution_context
-
-        def execution_context_callback():
-            pass
-
-        def _test_execution_context():
-            use_execution_context(
-                execution_context_callback, exec_ctx="invalid execution context"
-            )
-
-        self.assertRaises(AttributeError, render_hook, _test_execution_context)
 
 
 if __name__ == "__main__":
