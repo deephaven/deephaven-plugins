b87f4c416bc5bec8280cc7566c02c2d75c2b48c1
Matthew Runyon <matthewrunyon@deephaven.io>
2024-01-18 12:37:32 -0600

    Fix render update issue

--- plugins/ui/src/js/src/layout/ParentItemContext.ts
+++ plugins/ui/src/js/src/layout/ParentItemContext.ts
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useRef } from 'react';
 import { useLayoutManager } from '@deephaven/dashboard';
 import type { ContentItem } from '@deephaven/golden-layout';
 
@@ -7,9 +7,10 @@ export const ParentItemContext = createContext<ContentItem | null>(null);
 export function useParentItem() {
   const layoutManager = useLayoutManager();
   const parentContextItem = useContext(ParentItemContext);
-  return (
+  const parentItem = useRef(
     parentContextItem ??
-    layoutManager.root.contentItems[0] ??
-    layoutManager.root
+      layoutManager.root.contentItems[0] ??
+      layoutManager.root
   );
+  return parentItem.current;
 }
