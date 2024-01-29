0d5c3ce5e7d94cd6f0fe5275a015726bdb55b3e7
Colin Alworth <colinalworth@deephaven.io>
2024-01-29 10:28:00 -0600

    fix: use_execution_context does not return None (#232)

--- plugins/ui/src/deephaven/ui/hooks/use_execution_context.py
+++ plugins/ui/src/deephaven/ui/hooks/use_execution_context.py
@@ -23,13 +23,17 @@ def func_with_ctx(
         func()
 
 
-def use_execution_context(exec_ctx: ExecutionContext = None) -> None:
+def use_execution_context(
+    exec_ctx: ExecutionContext = None,
+) -> Callable[[Callable], None]:
     """
     Create an execution context wrapper for a function.
 
     Args:
         exec_ctx: ExecutionContext: The execution context to use. Defaults to
             the current execution context if not provided.
+    Returns:
+        A callable that will take any callable and invoke it within the current exec context
     """
     exec_ctx = use_memo(lambda: exec_ctx if exec_ctx else get_exec_ctx(), [exec_ctx])
     exec_fn = use_memo(lambda: partial(func_with_ctx, exec_ctx), [exec_ctx])
