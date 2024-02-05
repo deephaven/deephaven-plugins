ed7a6c7e0e3d40b67dda1ba5d043f140e7888577
Colin Alworth <colin@colinalworth.com>
2024-02-05 13:29:09 -0600

    Add unit test to confirm use_memo liveness behavior

--- plugins/ui/test/deephaven/ui/test_hooks.py
+++ plugins/ui/test/deephaven/ui/test_hooks.py
@@ -533,6 +533,62 @@ class HooksTest(BaseTestCase):
 
         render_hook(_test_execution_context)
 
+    def test_liveness_hook_interactions(self):
+        from deephaven.ui.hooks import use_memo, use_state
+        from deephaven import new_table, dtypes as dht
+        from deephaven.column import int_col
+        from deephaven.stream.table_publisher import table_publisher
+        from deephaven.update_graph import exclusive_lock
+        from deephaven.time import dh_now
+
+        # create a table publisher to mutate data outside the component
+        cols = {"X": dht.int32}
+        table, publisher = table_publisher(name="test table", col_defs=cols)
+        table = table.update("Timestamp=now()").with_attributes({"BlinkTable": True})
+
+        # tracking for use_state setters to mutate component state, to let us lock and confirm
+        setters = {}
+        local_rows = None
+
+        def _test_reused_tables():
+            """
+            Doesn't re-render size
+            """
+            a, setters["a"] = use_state(0)
+            # When "a" changes, recompute table - don't return or otherwise track this table w.r.t. liveness
+            nonlocal local_rows
+
+            def helper():
+                now = dh_now()
+                return table.where("Timestamp > now").last_by(by=["X"])
+
+            local_rows = use_memo(helper, {a})
+
+            return local_rows.size
+
+        # initial render, verify value is zero
+        rendered = render_hook(_test_reused_tables)
+        self.assertEqual(0, rendered["result"])
+
+        # render again, so that we drop the old liveness scope
+        result = rendered["rerender"]()
+        self.assertEqual(0, result)
+
+        # append a row, "a" stayed the same
+        publisher.add(new_table(cols=[int_col("X", [1])]))
+        # wait for the row to appear
+        with exclusive_lock(local_rows):
+            table.await_update(2_000)
+
+        # assert count correctly increased
+        result = rendered["rerender"]()
+        self.assertEqual(1, result)
+
+        # poke "a", verify the memoized table was replaced, no more rows
+        setters["a"](1)
+        result = rendered["rerender"]()
+        self.assertEqual(0, result)
+
 
 if __name__ == "__main__":
     unittest.main()
