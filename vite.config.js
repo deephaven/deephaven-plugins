3ed23c629e86227ee95d41252d4c32c5d21264d7
Brian Ingles <brianingles@deephaven.io>
2024-02-06 12:51:46 -0600

    Added config for STRICT_PORT
    #262

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import { defineConfig, loadEnv } from 'vite';
 const DEFAULT_PORT = 4100;
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
   const port = Number(env.PORT || process.env.PORT || DEFAULT_PORT);
+  const strictPort = (env.STRICT_PORT || process.env.STRICT_PORT) === 'true';
 
   // This config doesn't build anything, it just serves the files from the
   // plugins directory
@@ -13,6 +17,7 @@ export default defineConfig(({ mode }) => {
     publicDir: 'plugins',
     server: {
       port,
+      strictPort,
     },
   };
 });
