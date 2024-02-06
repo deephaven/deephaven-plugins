b87ab13a3050cbd5315153fbecd0ab3d791f8f7a
Colin Alworth <colin@colinalworth.com>
2024-02-05 20:03:49 -0600

    Add a use_state liveness test

--- plugins/ui/test/deephaven/ui/test_hooks.py
+++ plugins/ui/test/deephaven/ui/test_hooks.py
@@ -533,7 +533,69 @@ class HooksTest(BaseTestCase):
 
         render_hook(_test_execution_context)
 
-    def test_liveness_hook_interactions(self):
+    def test_liveness_use_state_interactions(self):
+        from deephaven.ui.hooks import use_memo, use_state
+        from deephaven import new_table, dtypes as dht
+        from deephaven.table import Table
+        from deephaven.column import int_col
+        from deephaven.stream import blink_to_append_only
+        from deephaven.stream.table_publisher import table_publisher
+        from deephaven.update_graph import exclusive_lock
+        from deephaven.time import dh_now
+
+        # create a table publisher to mutate data outside the component
+        cols = {"X": dht.int32}
+        table, publisher = table_publisher(name="test table", col_defs=cols)
+        table = blink_to_append_only(table).update("Timestamp=now()")
+
+        # tracking for use_state setters to mutate component state, to let us lock and confirm
+        set_a: Callable = lambda v: None
+        a: Table = None
+
+        def _test_reused_tables():
+            """
+            Doesn't re-render size
+            """
+            nonlocal a, set_a
+            a, set_a = use_state(lambda: table.where("X=1"))
+            # When "a" changes, recompute table - don't return or otherwise track this table w.r.t. liveness
+
+            return a.size
+
+        # initial render, verify value is zero
+        rendered = render_hook(_test_reused_tables)
+        self.assertEqual(0, rendered["result"])
+
+        # render again, so that we drop the old liveness scope
+        result = rendered["rerender"]()
+        self.assertEqual(0, result)
+
+        # append a row while we have the same table
+        publisher.add(new_table(cols=[int_col("X", [1])]))
+        # wait for the row to appear
+        with exclusive_lock(a):
+            table.await_update(2_000)
+
+        # assert count correctly increased
+        result = rendered["rerender"]()
+        self.assertEqual(1, result)
+
+        # replace the table with a new instance that must now be retained instead, back to zero
+        set_a(table.where("X=2"))
+        result = rendered["rerender"]()
+        self.assertEqual(0, result)
+
+        # add a row to that new table, ensure we see it
+        publisher.add(new_table(cols=[int_col("X", [2])]))
+        # wait for the row to appear
+        with exclusive_lock(a):
+            table.await_update(2_000)
+
+        # assert count correctly increased
+        result = rendered["rerender"]()
+        self.assertEqual(1, result)
+
+    def test_liveness_use_memo_interactions(self):
         from deephaven.ui.hooks import use_memo, use_state
         from deephaven import new_table, dtypes as dht
         from deephaven.column import int_col
@@ -547,14 +609,15 @@ class HooksTest(BaseTestCase):
         table = table.update("Timestamp=now()").with_attributes({"BlinkTable": True})
 
         # tracking for use_state setters to mutate component state, to let us lock and confirm
-        setters = {}
+        set_a: Callable = lambda v: None
         local_rows = None
 
         def _test_reused_tables():
             """
             Doesn't re-render size
             """
-            a, setters["a"] = use_state(0)
+            nonlocal set_a
+            a, set_a = use_state(0)
             # When "a" changes, recompute table - don't return or otherwise track this table w.r.t. liveness
             nonlocal local_rows
 
@@ -585,7 +648,7 @@ class HooksTest(BaseTestCase):
         self.assertEqual(1, result)
 
         # poke "a", verify the memoized table was replaced, no more rows
-        setters["a"](1)
+        set_a(1)
         result = rendered["rerender"]()
         self.assertEqual(0, result)
 
