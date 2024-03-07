4837b53a2ad9a700e38ff6b47b7fcb66c0d0032f
Steven Wu <wusteven815@gmail.com>
2024-03-07 12:27:55 -0500

    test: output logs

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -6,6 +6,7 @@ pushd "$(dirname "$0")"
 # Start the containers
 docker compose run --service-ports --rm --build "$@"
 exit_code=$?
+docker logs deephaven-plugins > /tmp/server-log.txt 2>&1
 docker compose down
 
 # Reset pwd
