174655d56d6978076431ffb4dec4ff65aca7992f
Steven Wu <wusteven815@gmail.com>
2024-03-18 09:31:34 -0400

    test: fix e2e logs (#344)
    - Fixes #345 
      - Move the log output command from the E2E YAML to the Docker script
    
    Since the Docker commands are being run in a new directory stack (by
    `pushd`), `docker logs` outside will show that the container is not
    found. Instead, the output is piped to a file in the `run_docker`
    script.

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -6,11 +6,13 @@ pushd "$(dirname "$0")"
 # Start the containers
 if [[ -z "${CI}" ]]; then
   docker compose run --service-ports --rm --build "$@"
+  exit_code=$?
+  docker compose down
 else
   docker compose run --service-ports --rm --build -e CI=true "$@"
+  exit_code=$?
+  docker compose stop deephaven-plugins
 fi
-exit_code=$?
-docker compose down
 
 # Reset pwd
 popd
