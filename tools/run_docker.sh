98877c56c3f94395196cd29621b4bf9b95f2091d
Steven Wu <wusteven815@gmail.com>
2024-04-04 09:24:07 -0400

    test: separate e2e compose (#375)
    - Fixes #376 
      - Separated the Docker compose containers for E2E to its own file

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -5,13 +5,14 @@ pushd "$(dirname "$0")"
 
 # Start the containers
 if [[ -z "${CI}" ]]; then
-  docker compose run --service-ports --rm --build "$@"
+  docker compose -f ../tests/docker-compose.yml run --service-ports --rm --build "$@"
   exit_code=$?
-  docker compose down
+  docker compose -f ../tests/docker-compose.yml down
 else
-  docker compose run --service-ports --rm --build -e CI=true "$@"
+  docker compose -f ../tests/docker-compose.yml run --service-ports --rm --build -e CI=true "$@"
   exit_code=$?
-  docker compose stop deephaven-plugins
+  # stop instead of down to preserve container logs
+  docker compose -f ../tests/docker-compose.yml stop deephaven-plugins
 fi
 
 # Reset pwd
