deephaven/deephaven-plugins

commit a4761cc046261d207e40d6cadef523411a1675c3

test: bump ts, eslint and prettier configs (#416)
- Bumps configs to latest versions
- Update failing types and formatting caught by newer versions

plugins/ui/src/js/src/elements/spectrum/Button.tsx
@@ -5,7 +5,10 @@ import {
 } from '@adobe/react-spectrum';
 import { SerializedButtonEventProps, useButtonProps } from './useButtonProps';
 
-function Button(props: SpectrumButtonProps & SerializedButtonEventProps) {
+function Button(
+  variant: SpectrumButtonProps['variant'],
+  props: SpectrumButtonProps & SerializedButtonEventProps
+): JSX.Element {
   const buttonProps = useButtonProps(props);
 
   // eslint-disable-next-line react/jsx-props-no-spreading

plugins/plotly-express/src/js/src/PlotlyExpressChartModel.ts
@@ -126,7 +126,7 @@ export class PlotlyExpressChartModel extends ChartModel {
     return this.layout;
   }
 
-  override close() {
+  override close(): void {
     super.close();
     this.widget?.close();
     this.widget = undefined;
@@ -180,7 +180,7 @@ export class PlotlyExpressChartModel extends ChartModel {
     this.widget = undefined;
   }
 
-  updateLayout(data: PlotlyChartWidgetData) {
+  updateLayout(data: PlotlyChartWidgetData): void {
     const { figure } = data;
     const { plotly } = figure;
     const { layout: plotlyLayout = {} } = plotly;
@@ -255,7 +255,7 @@ export class PlotlyExpressChartModel extends ChartModel {
     this.fireUpdate(this.getData());
   }
 
-  addTable(id: number, table: Table) {
+  addTable(id: number, table: Table): void {
     if (this.tableReferenceMap.has(id)) {
       return;
     }
@@ -267,7 +267,7 @@ export class PlotlyExpressChartModel extends ChartModel {
     }
   }
 
-  subscribeTable(id: number) {
+  subscribeTable(id: number): void {
     const table = this.tableReferenceMap.get(id);
     const columnReplacements = this.tableColumnReplacementMap.get(id);
 
@@ -292,7 +292,7 @@ export class PlotlyExpressChartModel extends ChartModel {
     }
   }
 
-  removeTable(id: number) {
+  removeTable(id: number): void {
     this.subscriptionCleanupMap.get(id)?.();
     this.tableSubscriptionMap.get(id)?.close();
 

plugins/plotly-express/src/js/src/PlotlyExpressChartPanel.tsx
@@ -7,7 +7,9 @@ import { useApi } from '@deephaven/jsapi-bootstrap';
 import PlotlyExpressChartModel from './PlotlyExpressChartModel.js';
 import { useHandleSceneTicks } from './useHandleSceneTicks.js';
 
-export function PlotlyExpressChartPanel(props: WidgetPanelProps<Widget>) {
+export function PlotlyExpressChartPanel(
+  props: WidgetPanelProps<Widget>
+): JSX.Element {
   const dh = useApi();
   const { fetch, metadata = {}, ...rest } = props;
   const containerRef = useRef<HTMLDivElement>(null);

plugins/plotly-express/src/js/src/PlotlyExpressChartUtils.ts
@@ -2,12 +2,12 @@ import type { Data, PlotlyDataLayoutConfig } from 'plotly.js';
 import type { Table, Widget } from '@deephaven/jsapi-types';
 
 export interface PlotlyChartWidget {
-  getDataAsBase64(): string;
-  exportedObjects: { fetch(): Promise<Table> }[];
-  addEventListener(
+  getDataAsBase64: () => string;
+  exportedObjects: { fetch: () => Promise<Table> }[];
+  addEventListener: (
     type: string,
     fn: (event: CustomEvent<PlotlyChartWidget>) => () => void
-  ): void;
+  ) => void;
 }
 
 export interface PlotlyChartWidgetData {

plugins/plotly-express/src/js/src/useHandleSceneTicks.ts
@@ -4,7 +4,7 @@ import PlotlyExpressChartModel from './PlotlyExpressChartModel.js';
 export function useHandleSceneTicks(
   model: PlotlyExpressChartModel | undefined,
   container: HTMLDivElement | null
-) {
+): void {
   useEffect(() => {
     // Plotly scenes and geo views reset when our data ticks
     // Pause rendering data updates when the user is manipulating a scene

plugins/ui/src/js/src/elements/ElementUtils.tsx
@@ -25,7 +25,7 @@ export type ObjectNode = {
  */
 export type ElementNode<
   K extends string = string,
-  P extends Record<string, unknown> = Record<string, unknown>
+  P extends Record<string, unknown> = Record<string, unknown>,
 > = {
   /**
    * The type of this element. Can be something like `deephaven.ui.components.Panel`, or
@@ -37,7 +37,7 @@ export type ElementNode<
 
 export type ElementNodeWithChildren<
   K extends string = string,
-  P extends Record<string, unknown> = Record<string, unknown>
+  P extends Record<string, unknown> = Record<string, unknown>,
 > = ElementNode<K, P> & {
   props: React.PropsWithChildren<P>;
 };

plugins/ui/src/js/src/elements/ObjectView.tsx
@@ -6,7 +6,7 @@ import type { dh } from '@deephaven/jsapi-types';
 const log = Log.module('@deephaven/js-plugin-ui/ObjectView');
 
 export type ObjectViewProps = { object: dh.WidgetExportedObject };
-function ObjectView(props: ObjectViewProps) {
+function ObjectView(props: ObjectViewProps): JSX.Element {
   const { object } = props;
   log.info('Object is', object);
   const { type } = object;

plugins/ui/src/js/src/elements/Picker.tsx
@@ -22,7 +22,7 @@ type WrappedDHPickerJSApiProps = Omit<DHPickerJSApiProps, 'table'> & {
 export type PickerProps = (DHPickerProps | WrappedDHPickerJSApiProps) &
   SerializedPickerEventProps;
 
-function Picker({ children, ...props }: PickerProps) {
+function Picker({ children, ...props }: PickerProps): JSX.Element {
   const settings = useSelector(getSettings<RootState>);
   const pickerProps = usePickerProps(props);
 

commit 24350b02e858234a995909ae0aa711aec47c5c43

fix: buttons not working due to extra prop
resolves #419

Broken by #416, extra prop slipped in while adding types and broke things. Removed the uncessary prop.

plugins/ui/src/js/src/elements/spectrum/Button.tsx
@@ -6,7 +6,6 @@ import {
 import { SerializedButtonEventProps, useButtonProps } from './useButtonProps';
 
 function Button(
-  variant: SpectrumButtonProps['variant'],
   props: SpectrumButtonProps & SerializedButtonEventProps
 ): JSX.Element {
   const buttonProps = useButtonProps(props);