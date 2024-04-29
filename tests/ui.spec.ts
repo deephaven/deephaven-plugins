177ed226a6b2ad5354122242062018ed50ad6e5e
mikebender <mikebender@deephaven.io>
2024-04-29 10:42:07 -0400

    WIP add e2e tests
    - They're not quite correct yet, but I want to refactor some other things first.

--- tests/ui.spec.ts
+++ tests/ui.spec.ts
@@ -6,3 +6,10 @@ test('UI loads', async ({ page }) => {
   await openPanel(page, 'ui_component', '.dh-react-panel');
   await expect(page.locator('.dh-react-panel')).toHaveScreenshot();
 });
+
+test('boom component shows an error in a panel', async ({ page }) => {
+  await gotoPage(page, '');
+  await openPanel(page, 'ui_boom', '.dh-react-panel');
+  await expect(page.locator('.dh-react-panel')).toBeVisible();
+  await expect(page.locator('.ui-error-view')).toHaveText('Boom!');
+});
