e6ac2d2e2b5c413f00c3e8be02aab3cb0ec0f221
mikebender <mikebender@deephaven.io>
2024-05-09 10:16:46 -0400

    Resolve review comment

--- plugins/ui/src/js/src/layout/ReactPanelContentOverlay.tsx
+++ plugins/ui/src/js/src/layout/ReactPanelContentOverlay.tsx
@@ -1,8 +1,7 @@
-/** A panel that uses the ReactPanelContentOverlayContext and if that content is set, renders it in a view with a partially transparent background */
-
 import React from 'react';
 import { usePanelContentOverlay } from './usePanelContentOverlay';
 
+/** A panel that uses the ReactPanelContentOverlayContext and if that content is set, renders it in a view with a partially transparent background */
 export function ReactPanelContentOverlay(): JSX.Element | null {
   const overlayContent = usePanelContentOverlay();
   return overlayContent != null ? (
