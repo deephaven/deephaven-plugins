40dfa23cbf1188d7c96f869116b2b19b52cb07c6
Ethan Alvizo <ethanalvizo@gmail.com>
2024-05-31 10:00:39 -0400

    docs: number field

--- plugins/ui/src/deephaven/ui/components/spectrum/__init__.py
+++ plugins/ui/src/deephaven/ui/components/spectrum/__init__.py
@@ -4,9 +4,10 @@ from .button_group import *
 from .button import *
 from .checkbox import *
 from .content import *
+from .contextual_help import *
+from .number_field import *
 from .range_slider import *
 from .slider import *
-from .contextual_help import *
 from .text_field import *
 from .toggle_button import *
 from .flex import *
