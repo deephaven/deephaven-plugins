c938c6b3515f14584beaff049baa41c816d35529
Brian Ingles <brianingles@deephaven.io>
2024-06-06 16:27:32 -0500

    Deleted SpectrumElementUtils and fixed index (#425)

--- plugins/ui/src/js/src/elements/utils/index.ts
+++ plugins/ui/src/js/src/elements/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './ElementUtils';
 export * from './EventUtils';
 export * from './HTMLElementUtils';
+export * from './IconElementUtils';
 export * from './UITableMouseHandler';
 export * from './UITableUtils';
