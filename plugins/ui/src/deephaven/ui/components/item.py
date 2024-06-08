a17ddb8d7c4b928fea7e54e0aecfeefb2defa43d
Ethan Alvizo <ethanalvizo@gmail.com>
2024-06-07 23:17:14 -0400

    type change

--- plugins/ui/src/deephaven/ui/components/item.py
+++ plugins/ui/src/deephaven/ui/components/item.py
@@ -10,9 +10,9 @@ ItemList = List[Item]
 
 def item(
     *children: Stringable,
-    title: Stringable | None = None,
-    text_value: Stringable | None = None,
-    aria_label: Stringable | None = None,
+    title: str | None = None,
+    text_value: str | None = None,
+    aria_label: str | None = None,
     child_items: ItemList | None = None,
     has_child_items: bool | None = None,
     **props: Any
@@ -23,7 +23,7 @@ def item(
     Args:
         children: The options to render within the item.
         title: Rendered contents of the item if `children` contains child items.
-        text_value: A string representatino of the item's contents, used for features like typeahead.
+        text_value: A string representation of the item's contents, used for features like typeahead.
         aria_label: An accessibility label for this item.
         child_items: A list of child items objects. Used for dynamic collections.
         has_child_items: Whether this item has children, even if not loaded yet.
