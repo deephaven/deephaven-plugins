60e66e5a3d41696d6a3512b786891aa16f8a3c11
Ethan Alvizo <ethanalvizo@gmail.com>
2024-06-07 21:50:02 -0400

    docs: item

--- plugins/ui/src/deephaven/ui/components/item.py
+++ plugins/ui/src/deephaven/ui/components/item.py
@@ -1,18 +1,32 @@
-from typing import Any, Union
+from typing import Any, Union, List
 
 from ..elements import BaseElement
 from ..types import Stringable
 
 ItemElement = BaseElement
 Item = Union[Stringable, ItemElement]
+ItemList = List[Item]
 
 
-def item(*children: Stringable, **props: Any) -> ItemElement:
+def item(
+    *children: Stringable,
+    title: Stringable | None = None,
+    text_value: Stringable | None = None,
+    aria_label: Stringable | None = None,
+    child_items: ItemList | None = None,
+    has_child_items: bool | None = None,
+    **props: Any
+) -> ItemElement:
     """
     An item that can be added to a menu, such as a picker
 
     Args:
         children: The options to render within the item.
+        title: Rendered contents of the item if `children` contains child items.
+        text_value: A string representatino of the item's contents, used for features like typeahead.
+        aria_label: An accessibility label for this item.
+        child_items: A list of child items objects. Used for dynamic collections.
+        has_child_items: Whether this item has childnre, even if not loaded yet.
         **props: Any other Item prop.
     """
     return BaseElement("deephaven.ui.components.Item", *children, **props)
