63e69a00c77f9f705d96797324add18efbbcd586
mikebender <mikebender@deephaven.io>
2024-06-13 15:39:49 -0400

    Revert "feat: Make RadioGroup orientation prop case insensitive (#536)"
    This reverts commit 9b0b4986ca9f59c7d474e4a6ae39d3def44542d6.

--- plugins/ui/src/js/src/elements/RadioGroup.tsx
+++ plugins/ui/src/js/src/elements/RadioGroup.tsx
@@ -2,38 +2,22 @@ import {
   RadioGroup as DHRadioGroup,
   RadioGroupProps as DHRadioGroupProps,
 } from '@deephaven/components';
-import { Orientation } from '@react-types/shared';
 import { SerializedFocusEventProps } from './SerializedPropTypes';
 import { useFocusEventCallback } from './spectrum/useFocusEventCallback';
 
-export type SerializedRadioGroupProps = Omit<
-  SerializedFocusEventProps<DHRadioGroupProps>,
-  'orientation'
-> & {
-  orientation?: Orientation | Uppercase<Orientation>;
-};
+export type SerializedRadioGroupProps =
+  SerializedFocusEventProps<DHRadioGroupProps>;
 
 function RadioGroup({
   onFocus: serializedOnFocus,
   onBlur: serializedOnBlur,
-  orientation: orientationMaybeUppercase,
   ...props
 }: SerializedRadioGroupProps): JSX.Element {
   const onFocus = useFocusEventCallback(serializedOnFocus);
   const onBlur = useFocusEventCallback(serializedOnBlur);
-  const orientationLc = orientationMaybeUppercase?.toLowerCase() as
-    | Orientation
-    | undefined;
 
-  return (
-    <DHRadioGroup
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...props}
-      onFocus={onFocus}
-      onBlur={onBlur}
-      orientation={orientationLc}
-    />
-  );
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <DHRadioGroup {...props} onFocus={onFocus} onBlur={onBlur} />;
 }
 
 export default RadioGroup;
