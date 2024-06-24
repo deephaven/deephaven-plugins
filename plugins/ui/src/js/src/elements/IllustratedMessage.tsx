ee4320db2df9464db58887d55b7fd82282de7aca
Ethan Alvizo <ethanalvizo@gmail.com>
2024-06-24 09:31:06 -0400

    wip

--- plugins/ui/src/js/src/elements/IllustratedMessage.tsx
+++ plugins/ui/src/js/src/elements/IllustratedMessage.tsx
@@ -1,30 +1,32 @@
+import React from 'react';
 import {
   IllustratedMessage as DHCAIllustratedMessage,
   IllustratedMessageProps as DHCAIllustratedMessageProps,
   Icon,
 } from '@deephaven/components';
 import { isElementOfType } from '@deephaven/react-hooks';
-import React from 'react';
 
 export function IllustratedMessage(
   props: DHCAIllustratedMessageProps
 ): JSX.Element {
   const { children, ...otherProps } = props;
 
-  const newChildren = React.Children.map(children, child => {
-    const isIcon = isElementOfType(child, Icon);
-    if (isIcon) {
-      if (child.props.size === undefined) child.props.size = 'XL';
+  if (children === undefined) return <DHCAIllustratedMessage {...props} />;
+
+  const childrenClone = React.Children.toArray(children);
+  const newChildren = React.Children.map(childrenClone, element => {
+    if (isElementOfType(element, Icon) === true) {
+      if (element.props.size === undefined) element.props.size = 'XL';
       if (
-        child.props.margin === undefined &&
-        child.props.marginY === undefined &&
-        child.props.marginBottom === undefined
+        element.props.margin === undefined &&
+        element.props.marginY === undefined &&
+        element.props.marginBottom === undefined
       ) {
-        child.props.marginBottom = 'size-10';
+        element.props.marginBottom = 'size-10';
       }
     }
 
-    return child;
+    return element;
   });
 
   return (
