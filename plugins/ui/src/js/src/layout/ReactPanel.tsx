b2c44dabd01153bdbe71442954f6ec5df95cf528
mikebender <mikebender@deephaven.io>
2024-06-25 11:54:51 -0400

    Don't render panel children if the widget has an error
    - We're just going to have a bad time.

--- plugins/ui/src/js/src/layout/ReactPanel.tsx
+++ plugins/ui/src/js/src/layout/ReactPanel.tsx
@@ -7,13 +7,7 @@ import {
   useLayoutManager,
   useListener,
 } from '@deephaven/dashboard';
-import {
-  View,
-  ViewProps,
-  Flex,
-  FlexProps,
-  ErrorBoundary,
-} from '@deephaven/components';
+import { View, ViewProps, Flex, FlexProps } from '@deephaven/components';
 import Log from '@deephaven/log';
 import PortalPanel from './PortalPanel';
 import { ReactPanelControl, useReactPanel } from './ReactPanelManager';
@@ -22,6 +16,8 @@ import { useParentItem } from './ParentItemContext';
 import { ReactPanelContext } from './ReactPanelContext';
 import { usePortalPanelManager } from './PortalPanelManagerContext';
 import ReactPanelContentOverlay from './ReactPanelContentOverlay';
+import ReactPanelErrorBoundary from './ReactPanelErrorBoundary';
+import usePanelContentOverlay from './usePanelContentOverlay';
 
 const log = Log.module('@deephaven/js-plugin-ui/ReactPanel');
 
@@ -172,6 +168,7 @@ function ReactPanel({
     },
     [parent, metadata, onOpen, panelId, title]
   );
+  const overlayContent = usePanelContentOverlay();
 
   return portal
     ? ReactDOM.createPortal(
@@ -205,8 +202,12 @@ function ReactPanel({
               rowGap={rowGap}
               columnGap={columnGap}
             >
-              {/* Have an ErrorBoundary around the children to display an error in the panel if there's any errors thrown when rendering the children */}
-              <ErrorBoundary>{children}</ErrorBoundary>
+              {/**
+               * Don't render the children if there's an error overlay. If there's an error with the widget, we can assume the children won't render properly.
+               */}
+              {overlayContent == null && (
+                <ReactPanelErrorBoundary>{children}</ReactPanelErrorBoundary>
+              )}
             </Flex>
           </View>
           <ReactPanelContentOverlay />
---
+++ plugins/ui/src/js/src/layout/ReactPanelErrorBoundary.tsx
@@ -0,0 +1,59 @@
+import Log from '@deephaven/log';
+import React, { Component, ReactNode } from 'react';
+import WidgetErrorView from '../widget/WidgetErrorView';
+
+const log = Log.module('ErrorBoundary');
+
+export interface ReactPanelErrorBoundaryProps {
+  /** Children to catch errors from. Error will reset when the children have been updated. */
+  children: ReactNode;
+}
+
+export interface ReactPanelErrorBoundaryState {
+  /** Currently displayed error. Reset when children are updated. */
+  error?: Error;
+}
+
+/**
+ * Error boundary for catching render errors in React. Displays an error message until the children have updated.
+ */
+export class ReactPanelErrorBoundary extends Component<
+  ReactPanelErrorBoundaryProps,
+  ReactPanelErrorBoundaryState
+> {
+  static getDerivedStateFromError(error: Error): ReactPanelErrorBoundaryState {
+    return { error };
+  }
+
+  constructor(props: ReactPanelErrorBoundaryProps) {
+    super(props);
+    this.state = { error: undefined };
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<ReactPanelErrorBoundaryProps>,
+    prevState: Readonly<ReactPanelErrorBoundaryState>
+  ): void {
+    const { children } = this.props;
+    if (prevProps.children !== children && prevState.error != null) {
+      log.debug(
+        'ReactPanelErrorBoundary clearing previous error',
+        prevState.error,
+        children
+      );
+      this.setState({ error: undefined });
+    }
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    log.error('Error caught by ErrorBoundary', error, errorInfo);
+  }
+
+  render(): ReactNode {
+    const { children } = this.props;
+    const { error } = this.state;
+    return error != null ? <WidgetErrorView error={error} /> : children;
+  }
+}
+
+export default ReactPanelErrorBoundary;
