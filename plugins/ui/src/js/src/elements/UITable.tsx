c4a2fe7fa3794db10d31b4e0b60ef4587a2b610c
Matthew Runyon <matthewrunyon@deephaven.io>
2024-07-05 16:14:19 -0500

    fix: ui.table cell and row press event data can be wrong (#593)
    Fixes #529 
    
    Based on refactor PR which drops the index from the callback #592. Need
    to rebase and drop the 1st 2 commits after that merges

--- plugins/ui/src/js/src/elements/UITable.tsx
+++ plugins/ui/src/js/src/elements/UITable.tsx
@@ -101,6 +101,7 @@ export function UITable({
         ? ([
             new UITableMouseHandler(
               model,
+              irisGrid,
               onCellPress,
               onCellDoublePress,
               onColumnPress,
