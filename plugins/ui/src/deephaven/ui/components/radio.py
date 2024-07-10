af23cdcd08341312dfabd56d1931631c2d4bcaf2
Ethan Alvizo <ethanalvizo@gmail.com>
2024-07-10 15:58:44 -0400

    added description

--- plugins/ui/src/deephaven/ui/components/radio.py
+++ plugins/ui/src/deephaven/ui/components/radio.py
@@ -83,7 +83,58 @@ def radio(
 
     Args:
         children: The label for the Radio. Accepts any renderable node.
-        **props: Any other Radio props.
+        value: The value of the radio button, used when submitting an HTML form.
+        is_disabled: Whether the radio button is disabled or not. Shows that a selection exists, but is not available in that circumstance.
+        auto_focus: Whether the radio button will be focused on the initial render.
+        on_focus: Handler that is called when the radio button is focused.
+        on_blur: Handler that is called when the radio button loses focus.
+        on_focus_change: Handler that is called when the radio button gains or loses focus.
+        on_key_down: Handler that is called when a key is pressed down.
+        on_key_up: Handler that is called when a key is released.
+        flex: When used in a flex layout, specifies how the element will grow or shrink to fit the space available.
+        flex_grow: When used in a flex layout, specifies how the element will grow to fit the space available.
+        flex_shrink: When used in a flex layout, specifies how the element will shrink to fit the space available.
+        flex_basis: When used in a flex layout, specifies the initial main size of the element.
+        align_self: Overrides the alignItems property of a flex or grid container.
+        justify_self: Species how the element is justified inside a flex or grid container.
+        order: The layout order for the element within a flex or grid container.
+        grid_area: When used in a grid layout specifies, specifies the named grid area that the element should be placed in within the grid.
+        grid_row: When used in a grid layout, specifies the row the element should be placed in within the grid.
+        grid_column: When used in a grid layout, specifies the column the element should be placed in within the grid.
+        grid_row_start: When used in a grid layout, specifies the starting row to span within the grid.
+        grid_row_end: When used in a grid layout, specifies the ending row to span within the grid.
+        grid_column_start: When used in a grid layout, specifies the starting column to span within the grid.
+        grid_column_end: When used in a grid layout, specifies the ending column to span within the grid.
+        margin: The margin for all four sides of the element.
+        margin_top: The margin for the top side of the element.
+        margin_bottom: The margin for the bottom side of the element.
+        margin_start: The margin for the logical start side of the element, depending on layout direction.
+        margin_end: The margin for the logical end side of the element, depending on layout direction.
+        margin_x: The margin for the left and right sides of the element.
+        margin_y: The margin for the top and bottom sides of the element.
+        width: The width of the element.
+        min_width: The minimum width of the element.
+        max_width: The maximum width of the element.
+        height: The height of the element.
+        min_height: The minimum height of the element.
+        max_height: The maximum height of the element.
+        position: The position of the element.
+        top: The distance from the top of the containing element.
+        bottom: The distance from the bottom of the containing element.
+        left: The distance from the left of the containing element.
+        right: The distance from the right of the containing element.
+        start: The distance from the start of the containing element, depending on layout direction.
+        end: The distance from the end of the containing element, depending on layout direction.
+        z_index: The stack order of the element.
+        is_hidden: Whether the element is hidden.
+        id: The unique identifier of the element.
+        aria-label: Defines a string value that labels the current element.
+        aria-labelledby: Identifies the element (or elements) that labels the current element.
+        aria-describedby: Identifies the element (or elements) that describes the object.
+        aria-details: Identifies the element (or elements) that provide a detailed, extended description for the object.
+        UNSAFE_class_name: Set the CSS className for the element. Only use as a last resort. Use style props instead.
+        UNSAFE_style: Set the inline style for the element. Only use as a last resort. Use style props instead.
+
     """
 
     children, props = create_props(locals())
--- plugins/ui/src/deephaven/ui/components/radio_group.py
+++ plugins/ui/src/deephaven/ui/components/radio_group.py
@@ -99,6 +99,71 @@ def radio_group(
 
     Args:
         children: The Radio(s) contained within the RadioGroup.
+        is_emphasized: By default, radio button are not emphasized (gray). The emphasized version provides visual prominence.
+        orientation: The axis the Radio Buttons should align with.
+        value: The value of the selected radio button.
+        default_value: The default value of the radio button.
+        is_disabled: Whether the radio button is disabled.
+        is_read_only: Whether the radio button can be selected but not changed by the user.
+        name: The name of the radio button, used when submitting and HTML form.
+        is_required: Whether the radio button is required on the input before form submission.
+        is_invalid: Whether the radio button is in an invalid state.
+        label: The content to display as the label.
+        description: A description for the field. Provides a hint such as specific requirements for what to choose.
+        error_message: An error message for the field.
+        label_position: The position of the label relative to the radio button.
+        label_align: The horizontal alignment of the label relative to the radio button.
+        necessity_indicator: Whether the required state should be shown as an icon or text.
+        contextual_help: A ContextualHelp element to place next to the label
+        on_change: Handler that is called when the radio button value changes.
+        on_focus: Handler that is called when the radio button is focused.
+        on_blur: Handler that is called when the radio button loses focus.
+        on_focus_change: Handler that is called when the radio button gains or loses focus.
+        flex: When used in a flex layout, specifies how the element will grow or shrink to fit the space available.
+        flex_grow: When used in a flex layout, specifies how the element will grow to fit the space available.
+        flex_shrink: When used in a flex layout, specifies how the element will shrink to fit the space available.
+        flex_basis: When used in a flex layout, specifies the initial main size of the element.
+        align_self: Overrides the alignItems property of a flex or grid container.
+        justify_self: Species how the element is justified inside a flex or grid container.
+        order: The layout order for the element within a flex or grid container.
+        grid_area: When used in a grid layout specifies, specifies the named grid area that the element should be placed in within the grid.
+        grid_row: When used in a grid layout, specifies the row the element should be placed in within the grid.
+        grid_column: When used in a grid layout, specifies the column the element should be placed in within the grid.
+        grid_row_start: When used in a grid layout, specifies the starting row to span within the grid.
+        grid_row_end: When used in a grid layout, specifies the ending row to span within the grid.
+        grid_column_start: When used in a grid layout, specifies the starting column to span within the grid.
+        grid_column_end: When used in a grid layout, specifies the ending column to span within the grid.
+        margin: The margin for all four sides of the element.
+        margin_top: The margin for the top side of the element.
+        margin_bottom: The margin for the bottom side of the element.
+        margin_start: The margin for the logical start side of the element, depending on layout direction.
+        margin_end: The margin for the logical end side of the element, depending on layout direction.
+        margin_x: The margin for the left and right sides of the element.
+        margin_y: The margin for the top and bottom sides of the element.
+        width: The width of the element.
+        min_width: The minimum width of the element.
+        max_width: The maximum width of the element.
+        height: The height of the element.
+        min_height: The minimum height of the element.
+        max_height: The maximum height of the element.
+        position: The position of the element.
+        top: The distance from the top of the containing element.
+        bottom: The distance from the bottom of the containing element.
+        left: The distance from the left of the containing element.
+        right: The distance from the right of the containing element.
+        start: The distance from the start of the containing element, depending on layout direction.
+        end: The distance from the end of the containing element, depending on layout direction.
+        z_index: The stack order of the element.
+        is_hidden: Whether the element is hidden.
+        id: The unique identifier of the element.
+        aria-label: Defines a string value that labels the current element.
+        aria-labelledby: Identifies the element (or elements) that labels the current element.
+        aria-describedby: Identifies the element (or elements) that describes the object.
+        aria-details: Identifies the element (or elements) that provide a detailed, extended description for the object.
+        aria_errormessage: Identifies the element that provides an error message for the object.
+        UNSAFE_class_name: Set the CSS className for the element. Only use as a last resort. Use style props instead.
+        UNSAFE_style: Set the inline style for the element. Only use as a last resort. Use style props instead.
+
     """
 
     children, props = create_props(locals())
