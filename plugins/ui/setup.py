a2459bdd97cfab6fbe3819f4aa7312477473afe6
Mike Bender <mikebender@deephaven.io>
2024-07-12 14:22:09 -0400

    build: Clean the build directory before building the wheel (#599)
    - When rebuilding the wheel, it had stale files for the _js files, which
    was confusing and annoying
    - Clean the build directory so it doesn't have stale files
    - Still need to run `npm run build` between builds
    - Fixes #380

--- plugins/ui/setup.py
+++ plugins/ui/setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 import os
+import shutil
 from deephaven.plugin.packaging import package_js
 
 js_dir = "src/js/"
 dest_dir = os.path.join("src/deephaven/ui/_js")
+
+# remove the build/dist directory to ensure that the package is built from the latest js files
+try:
+    shutil.rmtree("build")
+    shutil.rmtree("dist")
+except FileNotFoundError:
+    pass
 
 package_js(js_dir, dest_dir)
 
