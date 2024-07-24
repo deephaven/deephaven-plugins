6d8433e2c14243c76e0118f29b6f124cc1d30475
David Godinez <davidgodinez@deephaven.io>
2024-07-24 14:07:12 -0600

    ui.date_picker JS side implementation

---
+++ plugins/ui/src/js/src/elements/DatePicker.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useState } from 'react';
+import {
+  DatePicker as DHCDatePicker,
+  DatePickerProps as DHCDatePickerProps,
+} from '@deephaven/components';
+import { useDebouncedCallback } from '@deephaven/react-hooks';
+// TODO get from components
+import {
+  CalendarDate,
+  CalendarDateTime,
+  ZonedDateTime,
+} from '@internationalized/date';
+import {
+  SerializedDatePickerProps,
+  useDatePickerProps,
+} from './hooks/useDatepickerProps';
+
+// TODO delete
+type DateValue = CalendarDate | CalendarDateTime | ZonedDateTime;
+
+const VALUE_CHANGE_DEBOUNCE = 250;
+
+const EMPTY_FUNCTION = () => undefined;
+
+export function DatePicker(
+  props: SerializedDatePickerProps<DHCDatePickerProps<DateValue>>
+): JSX.Element {
+  const {
+    defaultValue = null,
+    value: propValue,
+    onChange: propOnChange = EMPTY_FUNCTION,
+    ...otherProps
+  } = useDatePickerProps(props);
+
+  const [value, setValue] = useState(propValue ?? defaultValue);
+
+  const debouncedOnChange = useDebouncedCallback(
+    propOnChange,
+    VALUE_CHANGE_DEBOUNCE
+  );
+
+  const onChange = useCallback(
+    newValue => {
+      setValue(newValue);
+      debouncedOnChange(newValue);
+    },
+    [debouncedOnChange]
+  );
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <DHCDatePicker value={value} onChange={onChange} {...otherProps} />
+  );
+}
+
+DatePicker.displayName = 'DatePicker';
+
+export default DatePicker;
---
+++ plugins/ui/src/js/src/elements/hooks/useDatepickerProps.ts
@@ -0,0 +1,138 @@
+import { useCallback } from 'react';
+// TODO delete
+import {
+  CalendarDate,
+  CalendarDateTime,
+  DateValue,
+  ZonedDateTime,
+} from '@internationalized/date';
+import {
+  DeserializedFocusEventCallback,
+  SerializedFocusEventCallback,
+  useFocusEventCallback,
+} from './useFocusEventCallback';
+import {
+  DeserializedKeyboardEventCallback,
+  SerializedKeyboardEventCallback,
+  useKeyboardEventCallback,
+} from './useKeyboardEventCallback';
+
+export type SerializedDateValue = string | null;
+
+export type SerializedDateValueCallback = (value: SerializedDateValue) => void;
+
+type MappedDateValue<T> = T extends ZonedDateTime
+  ? ZonedDateTime
+  : T extends CalendarDateTime
+  ? CalendarDateTime
+  : T extends CalendarDate
+  ? CalendarDate
+  : never;
+export type DeserializedDateValueCallback = (
+  value: MappedDateValue<DateValue>
+) => void;
+
+export interface SerializedDatePickerEventProps {
+  /** Handler that is called when the element receives focus. */
+  onFocus?: SerializedFocusEventCallback;
+
+  /** Handler that is called when the element loses focus. */
+  onBlur?: SerializedFocusEventCallback;
+
+  /** Handler that is called when a key is pressed */
+  onKeyDown?: SerializedKeyboardEventCallback;
+
+  /** Handler that is called when a key is released */
+  onKeyUp?: SerializedKeyboardEventCallback;
+
+  /** Handler that is called when the value changes */
+  onChange?: SerializedDateValueCallback;
+}
+
+export interface DeserializedDatePickerEventProps {
+  /** Handler that is called when the element receives focus. */
+  onFocus?: DeserializedFocusEventCallback;
+
+  /** Handler that is called when the element loses focus. */
+  onBlur?: DeserializedFocusEventCallback;
+
+  /** Handler that is called when a key is pressed */
+  onKeyDown?: DeserializedKeyboardEventCallback;
+
+  /** Handler that is called when a key is released */
+  onKeyUp?: DeserializedKeyboardEventCallback;
+
+  /** Handler that is called when the value changes */
+  onChange?: DeserializedDateValueCallback;
+}
+
+export type SerializedDatePickerProps<TProps> = TProps &
+  SerializedDatePickerEventProps;
+
+export type DeserializedDatePickerProps<TProps> = Omit<
+  TProps,
+  keyof SerializedDatePickerEventProps
+> &
+  DeserializedDatePickerEventProps;
+
+/**
+ * Uses the toString representiation of the DateValue as the serialized value.
+ * @param value DateValue to serialize
+ * @returns Serialized DateValue
+ */
+export function serializeDateValue(
+  value: MappedDateValue<DateValue>
+): SerializedDateValue {
+  if (value == null) {
+    return null;
+  }
+
+  return value.toString();
+}
+
+/**
+ * Get a callback function that can be passed to the onChange event handler
+ * props of a Spectrum DatePicker.
+ * @param callback Callback to be called with the serialized value
+ * @returns A callback to be passed into the Spectrum component that transforms
+ * the value and calls the provided callback
+ */
+export function useOnChangeCallback(
+  callback?: SerializedDateValueCallback
+): (value: MappedDateValue<DateValue>) => void {
+  return useCallback(
+    (value: MappedDateValue<DateValue>) => {
+      callback?.(serializeDateValue(value));
+    },
+    [callback]
+  );
+}
+
+/**
+ * Wrap DatePicker props with the appropriate serialized event callbacks.
+ * @param props Props to wrap
+ * @returns Wrapped props
+ */
+export function useDatePickerProps<TProps>({
+  onFocus,
+  onBlur,
+  onKeyDown,
+  onKeyUp,
+  onChange: serializedOnChange,
+  ...otherProps
+}: SerializedDatePickerProps<TProps>): DeserializedDatePickerProps<TProps> {
+  const serializedOnFocus = useFocusEventCallback(onFocus);
+  const serializedOnBlur = useFocusEventCallback(onBlur);
+  const serializedOnKeyDown = useKeyboardEventCallback(onKeyDown);
+  const serializedOnKeyUp = useKeyboardEventCallback(onKeyUp);
+  const onChange = useOnChangeCallback(serializedOnChange);
+
+  return {
+    onFocus: serializedOnFocus,
+    onBlur: serializedOnBlur,
+    onKeyDown: serializedOnKeyDown,
+    onKeyUp: serializedOnKeyUp,
+    onChange: serializedOnChange == null ? undefined : onChange,
+    ...otherProps,
+  };
+}
