8738c3abbec6e9f81ebafda11f73eb0152c110a1
Joe Numainville <josephnumainville@deephaven.io>
2024-07-24 13:22:08 -0500

    wip

--- tests/express.spec.ts
+++ tests/express.spec.ts
@@ -12,3 +12,9 @@ test('Plotly loads', async ({ page }) => {
   await openPanel(page, 'plotly_fig', '.js-plotly-plot');
   await expect(page.locator('.iris-chart-panel')).toHaveScreenshot();
 });
+
+test('Histogram loads', async ({ page }) => {
+  await gotoPage(page, '');
+  await openPanel(page, 'express_hist_by', '.js-plotly-plot');
+  await expect(page.locator('.iris-chart-panel')).toHaveScreenshot();
+});
