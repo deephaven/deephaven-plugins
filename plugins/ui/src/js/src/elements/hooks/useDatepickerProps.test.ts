863e20d7d8cb00fdc8c9948a96b15cd2d36bc94f
David Godinez <davidgodinez@deephaven.io>
2024-07-30 10:06:41 -0600

    update unit tests

--- plugins/ui/src/js/src/elements/hooks/useDatepickerProps.test.ts
+++ plugins/ui/src/js/src/elements/hooks/useDatepickerProps.test.ts
@@ -1,46 +1,67 @@
 import { parseDateValue, parseNullableDateValue } from './useDatepickerProps';
 
+const DEFAULT_TIME_ZONE = 'UTC';
+const NY_TIME_ZONE = 'America/New_York';
+
 describe('parseDateValue', () => {
   const isoDate = '2021-02-03';
   const isoDateTime = '2021-03-03T04:05:06';
   const isoZonedDateTime = '2021-04-04T05:06:07-04:00[America/New_York]';
   const nonIsoZonedDateTime = '2021-04-04T05:06:07 America/New_York';
   const instantString = '2021-03-03T04:05:06Z';
+  const instantStringUTC = '2021-03-03T04:05:06Z[UTC]';
+  const utcOutput = '2021-03-03T04:05:06+00:00[UTC]';
+  const nyOutput = '2021-03-02T23:05:06-05:00[America/New_York]';
   const invalidDate = 'invalid-date';
 
   it('should return null if the value is null', () => {
-    expect(parseNullableDateValue(null)).toBeNull();
+    expect(parseNullableDateValue(DEFAULT_TIME_ZONE, null)).toBeNull();
   });
 
   it('should return undefined if the value is undefined', () => {
-    expect(parseDateValue(undefined)).toBeUndefined();
+    expect(parseDateValue(DEFAULT_TIME_ZONE, undefined)).toBeUndefined();
   });
 
   it('should parse an ISO 8601 date string', () => {
-    expect(parseDateValue(isoDate)?.toString()).toEqual(isoDate);
+    expect(parseDateValue(DEFAULT_TIME_ZONE, isoDate)?.toString()).toEqual(
+      isoDate
+    );
   });
 
   it('should parse an ISO 8601 date time string', () => {
-    expect(parseDateValue(isoDateTime)?.toString()).toEqual(isoDateTime);
+    expect(parseDateValue(DEFAULT_TIME_ZONE, isoDateTime)?.toString()).toEqual(
+      isoDateTime
+    );
   });
 
   it('should parse an ISO 8601 zoned date time string', () => {
-    expect(parseDateValue(isoZonedDateTime)?.toString()).toEqual(
-      isoZonedDateTime
-    );
+    expect(
+      parseDateValue(DEFAULT_TIME_ZONE, isoZonedDateTime)?.toString()
+    ).toEqual(isoZonedDateTime);
   });
 
   it('should parse a non-ISO 8601 zoned date time string', () => {
-    expect(parseDateValue(nonIsoZonedDateTime)?.toString()).toEqual(
-      isoZonedDateTime
-    );
+    expect(
+      parseDateValue(DEFAULT_TIME_ZONE, nonIsoZonedDateTime)?.toString()
+    ).toEqual(isoZonedDateTime);
   });
 
   it('should parse an instant string', () => {
-    expect(parseDateValue(instantString)?.toString()).toEqual(isoDateTime);
+    expect(
+      parseDateValue(DEFAULT_TIME_ZONE, instantString)?.toString()
+    ).toEqual(utcOutput);
+    expect(
+      parseDateValue(DEFAULT_TIME_ZONE, instantStringUTC)?.toString()
+    ).toEqual(utcOutput);
   });
 
   it('should throw an error if the value is invalid', () => {
-    expect(() => parseDateValue(invalidDate)).toThrow();
+    expect(() => parseDateValue(DEFAULT_TIME_ZONE, invalidDate)).toThrow();
+  });
+
+  it('should parse an instant time string with a different time zone', () => {
+    expect(parseDateValue(NY_TIME_ZONE, instantString)?.toString()).toEqual(
+      nyOutput
+    );
   });
 });
