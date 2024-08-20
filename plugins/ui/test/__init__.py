f182551ba5dbbc487365fef0ca96bb47d0b3ddba
mikebender <mikebender@deephaven.io>
2024-08-20 10:08:06 -0400

    Try/catch around starting server

--- plugins/ui/test/__init__.py
+++ plugins/ui/test/__init__.py
@@ -1,8 +1,13 @@
 from deephaven_server.server import Server
 
-# Make sure the server is running first thing
+# Try and start the server if it's not already running
 # Otherwise we get errors whenever we try to import anything or run tests
-if Server.instance is None:
-    # Use port 11000 so it doesn't conflict with another server
-    s = Server(port=11000, jvm_args=["-Xmx4g"])
-    s.start()
+try:
+    if Server.instance is None:
+        # Use port 11000 so it doesn't conflict with another server
+        s = Server(port=11000, jvm_args=["-Xmx4g"])
+        s.start()
+except Exception as e:
+    #  Even if starting the server throws, we want to continue to try and run our tests
+    # We don't _really_ need a server running to run our tests, we just need the server to be running to import the deephaven namespace
+    pass
