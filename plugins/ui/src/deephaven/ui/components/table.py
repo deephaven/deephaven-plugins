d9b085f33d30b54de569f7aa137092978520f0b1
Matthew Runyon <matthewrunyon@deephaven.io>
2024-08-29 15:50:31 -0500

    Fix CI error

--- plugins/ui/src/deephaven/ui/components/table.py
+++ plugins/ui/src/deephaven/ui/components/table.py
@@ -81,6 +81,9 @@ def table(
         context_header_menu: The context menu items to show when a column header is right clicked.
             May contain action items or submenu items.
             May also be a function that receives the column header data and returns the context menu items or None.
+
+    Returns:
+        The rendered Table.
     """
     props = locals()
     del props["table"]
