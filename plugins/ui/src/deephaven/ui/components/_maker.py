1c17db0a138317b77679f102f50a39a216910523
Steven Wu <wusteven815@gmail.com>
2024-09-09 14:35:31 -0400

    feat: experimental element maker

---
+++ plugins/ui/src/deephaven/ui/components/_maker.py
@@ -0,0 +1,47 @@
+from typing import Any
+from inspect import signature
+from functools import wraps
+from ..elements import BaseElement
+
+params = {
+    "wrap": "Whether children should wrap when they exceed the panel's width.",
+    "justify_content": "The distribution of space around items along the main axis.",
+    "align_content": "The distribution of space between and around items along the cross axis.",
+    "align_items": "The alignment of children within their container.",
+    "gap": "The space to display between both rows and columns of children.",
+    "column_gap": "The space to display between columns of children.",
+    "row_gap": "The space to display between rows of children.",
+    "key": "A unique key for the element",
+}
+
+
+def make_element(
+    description: str,
+    override_params: dict[str, str] = {},
+    return_string: str | None = None,
+):
+    def decorator(f: Any):
+        nonlocal return_string
+
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            props = f(*args, **kwargs)
+            return BaseElement(f"deephaven.ui.components.{f.__name__}", **props)
+
+        sig = signature(f)
+        if return_string is None:
+            return_string = f"The rendered {f.__name__.replace('_', ' ')} component."
+
+        wrapper.__doc__ = f"{description}\n\nArgs:\n"
+        for name in (param.name for param in sig.parameters.values()):
+            if name in override_params:
+                wrapper.__doc__ += f"    {name}: {override_params[name]}\n"
+            elif name in params:
+                wrapper.__doc__ += f"    {name}: {params[name]}\n"
+            else:
+                raise ValueError(f'Docs of parameter "{name}" not specified.')
+        wrapper.__doc__ += f"\nReturns:\n    {return_string}\n"
+
+        return wrapper
+
+    return decorator
--- plugins/ui/src/deephaven/ui/components/flex.py
+++ plugins/ui/src/deephaven/ui/components/flex.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 from typing import Any
-from inspect import signature, Parameter
+from inspect import signature
 from functools import wraps
-from .basic import component_element
 from .types import (
     LayoutFlex,
     Direction,
@@ -12,41 +11,22 @@ from .types import (
     AlignItems,
     DimensionValue,
 )
+from ._maker import make_element
+
+_override_params = {
+    "children": "Elements to render in the flexbox.",
+    "flex": "The flex property of the flexbox.",
+    "direction": "The direction of the flexbox.",
+}
 
 
-def wrap_prop(f: Any, wrapper: Any, desc: str) -> Any:
-    sig = signature(f)
-    params = list(sig.parameters.values())
-    wrapper_params = list(signature(wrapper).parameters.values())[1:-1]
-
-    for wrapper_param in wrapper_params:
-        params.insert(1, wrapper_param)
-
-    wrapper = wraps(f)(wrapper)
-    wrapper.__signature__ = sig.replace(parameters=params)
-    wrapper.__doc__ += f"        {desc}\n"
-
-    return wrapper
-
-
-def add_key_prop(f: Any) -> Any:
-    def wrapper(*args, key: str | None = None, **kwargs):
-        return f(*args, key=key, **kwargs)
-
-    return wrap_prop(f, wrapper, "key: A unique key for the component.")
-
-
-def add_qwerty_prop(f: Any) -> Any:
-    def wrapper(*args, qwerty: int | None = None, **kwargs):
-        return f(*args, qwerty=qwerty, **kwargs)
-
-    return wrap_prop(f, wrapper, "qwerty: qwerty.")
-
-
-@add_qwerty_prop
-@add_key_prop
+@make_element(
+    "Base Flex component for laying out children in a flexbox.",
+    override_params=_override_params,
+)
 def flex(
     *children: Any,
+    key: str | None = None,
     flex: LayoutFlex | None = "auto",
     direction: Direction | None = None,
     wrap: Wrap | None = None,
@@ -56,34 +36,5 @@ def flex(
     gap: DimensionValue | None = "size-100",
     column_gap: DimensionValue | None = None,
     row_gap: DimensionValue | None = None,
-    **props: Any,
 ):
-    """
-    Base Flex component for laying out children in a flexbox.
-
-    Args:
-        *children: Elements to render in the flexbox.
-        flex: The flex property of the flexbox.
-        direction: The direction in which to layout children.
-        wrap: Whether children should wrap when they exceed the panel's width.
-        justify_content: The distribution of space around items along the main axis.
-        align_content: The distribution of space between and around items along the cross axis.
-        align_items: The alignment of children within their container.
-        gap: The space to display between both rows and columns of children.
-        column_gap: The space to display between columns of children.
-        row_gap: The space to display between rows of children.
-    """
-    return component_element(
-        "Flex",
-        *children,
-        flex=flex,
-        direction=direction,
-        wrap=wrap,
-        justify_content=justify_content,
-        align_content=align_content,
-        align_items=align_items,
-        gap=gap,
-        column_gap=column_gap,
-        row_gap=row_gap,
-        **props,
-    )
+    return locals()
