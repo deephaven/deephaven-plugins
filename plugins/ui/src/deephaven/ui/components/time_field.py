c581afa145275da29cbf0e2c437b5443f618db96
David Godinez <davidgodinez@deephaven.io>
2024-09-20 12:57:12 -0600

    add key

--- plugins/ui/src/deephaven/ui/components/time_field.py
+++ plugins/ui/src/deephaven/ui/components/time_field.py
@@ -145,6 +145,7 @@ def time_field(
     aria_details: str | None = None,
     UNSAFE_class_name: str | None = None,
     UNSAFE_style: CSSProperties | None = None,
+    key: str | None = None,
 ) -> TimeFieldElement:
     """
     A time field allows the user to select a time.
@@ -235,6 +236,7 @@ def time_field(
         aria_details: The details for the element.
         UNSAFE_class_name: A CSS class to apply to the element.
         UNSAFE_style: A CSS style to apply to the element.
+        key: A unique identifier used by React to render elements in a list.
 
     Returns:
         The time field element.
