5581ae4d891eefc38ec7e7b7960849c288808859
Akshat Jawne <69530774+AkshatJawne@users.noreply.github.com>
2024-09-21 03:39:11 -0400

    fix: ui.radio value defaulting (#818)
    Closes #765

--- plugins/ui/src/js/src/elements/Radio.tsx
+++ plugins/ui/src/js/src/elements/Radio.tsx
@@ -18,6 +18,8 @@ export function Radio({
   onBlur: serializedOnBlur,
   onKeyDown: serializedOnKeyDown,
   onKeyUp: serializedOnKeyUp,
+  value: valueProp,
+  children,
   ...props
 }: SerializedRadioProps): JSX.Element {
   const onFocus = useFocusEventCallback(serializedOnFocus);
@@ -25,15 +27,20 @@ export function Radio({
   const onKeyDown = useKeyboardEventCallback(serializedOnKeyDown);
   const onKeyUp = useKeyboardEventCallback(serializedOnKeyUp);
 
+  const value = valueProp ?? (typeof children === 'string' ? children : '');
+
   return (
     <DHRadio
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
+      value={value}
       onFocus={onFocus}
       onBlur={onBlur}
       onKeyDown={onKeyDown}
       onKeyUp={onKeyUp}
-    />
+    >
+      {children}
+    </DHRadio>
   );
 }
 
