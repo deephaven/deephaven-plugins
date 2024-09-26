85b7edf918d5ee439ed12b43c4774e775602b2dd
Steven Wu <wusteven815@gmail.com>
2024-09-26 10:55:00 -0400

    add code component

--- plugins/ui/src/js/src/elements/Markdown.tsx
+++ plugins/ui/src/js/src/elements/Markdown.tsx
@@ -1,10 +1,32 @@
+import React from 'react';
+import { type CodeComponent } from 'react-markdown/lib/ast-to-react';
 import remarkMath from 'remark-math';
 import rehypeMathjax from 'rehype-mathjax';
 import ReactMarkdown from 'react-markdown';
 import { View, ViewProps } from '@deephaven/components';
+import { Code } from '@deephaven/console';
 
 type MarkdownProps = Omit<ViewProps, 'children'> & {
   children: string;
+};
+
+const renderMarkdown: CodeComponent = props => {
+  const { children, className } = props;
+  const language =
+    className !== undefined && className?.startsWith('language-')
+      ? className.substring(9)
+      : 'plaintext';
+  return (
+    <pre>
+      <code>
+        <Code language={language}>
+          {React.Children.map(children, child =>
+            typeof child === 'string' ? child.trim() : child
+          )}
+        </Code>
+      </code>
+    </pre>
+  );
 };
 
 export function Markdown({ children, ...props }: MarkdownProps): JSX.Element {
@@ -12,6 +34,7 @@ export function Markdown({ children, ...props }: MarkdownProps): JSX.Element {
     // eslint-disable-next-line react/jsx-props-no-spreading
     <View {...props}>
       <ReactMarkdown
+        components={{ code: renderMarkdown }}
         remarkPlugins={[remarkMath]}
         rehypePlugins={[rehypeMathjax]}
       >
