33ef045ffffb68025263046d1b5cb31db885dd20
David Godinez <davidgodinez@deephaven.io>
2024-10-08 08:18:53 -0600

    add debounce

--- plugins/ui/src/js/src/elements/RangeCalendar.tsx
+++ plugins/ui/src/js/src/elements/RangeCalendar.tsx
@@ -8,14 +8,28 @@ import {
   SerializedRangeCalendarProps,
   useRangeCalendarProps,
 } from './hooks/useRangeCalendarProps';
+import { RangeValue } from './hooks';
+import useDebouncedOnChange from './hooks/useDebouncedOnChange';
+
+const EMPTY_FUNCTION = () => undefined;
 
 export function RangeCalendar(
   props: SerializedRangeCalendarProps<DHCRangeCalendarProps<DateValue>>
 ): JSX.Element {
-  const rangeCalendarProps = useRangeCalendarProps(props);
+  const {
+    defaultValue = null,
+    value: propValue,
+    onChange: propOnChange = EMPTY_FUNCTION,
+    ...otherProps
+  } = useRangeCalendarProps(props);
+
+  const [value, onChange] = useDebouncedOnChange<RangeValue<DateValue> | null>(
+    propValue ?? defaultValue,
+    propOnChange
+  );
 
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <DHCRangeCalendar {...rangeCalendarProps} />;
+  return <DHCRangeCalendar value={value} onChange={onChange} {...otherProps} />;
 }
 
 RangeCalendar.displayName = 'RangeCalendar';
