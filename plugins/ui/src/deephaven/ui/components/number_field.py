acff01858db0cec5fcc61d11191be1ddad003843
Steven Wu <wusteven815@gmail.com>
2024-10-21 14:01:16 -0400

    add None to necessity_indicator

--- plugins/ui/src/deephaven/ui/components/number_field.py
+++ plugins/ui/src/deephaven/ui/components/number_field.py
@@ -44,7 +44,7 @@ def number_field(
     name: str | None = None,
     label_position: LabelPosition = "top",
     label_align: Alignment | None = None,
-    necessity_indicator: NecessityIndicator = "icon",
+    necessity_indicator: NecessityIndicator | None = None,
     contextual_help: Any | None = None,
     on_focus: FocusEventCallable | None = None,
     on_blur: FocusEventCallable | None = None,
