ef203191e340bc2eaacb371c0efe385135b58af0
Steven Wu <wusteven815@gmail.com>
2024-11-05 11:55:38 -0500

    test

---
+++ tools/check_typescipt_ci.py
@@ -0,0 +1,34 @@
+from subprocess import run
+from re import fullmatch
+
+
+def main():
+    print("Checking TypeScript types...")
+    res = run(
+        ["npx", "tsc", "-p", ".", "--emitDeclarationOnly", "false", "--noEmit"],
+        capture_output=True,
+    )
+
+    if res.returncode == 0:
+        return
+
+    errors = []
+    for line in res.stdout.decode("utf-8").splitlines():
+        if len(line) == 0:
+            continue
+        if line[0] == " " and len(errors) > 0:
+            errors[-1] += "\n" + line
+        else:
+            errors.append(line)
+
+    for err in errors:
+        match = fullmatch(r"(.+?)\((\d+),(\d+)\): ([\s\S]+)", err)
+        if match is None:
+            continue
+
+        file, line, col, msg = match.groups()
+        print(f"::error file={file},line={line},col={col}::{msg}")
+
+
+if __name__ == "__main__":
+    main()
