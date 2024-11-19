38f57454fac8deff4362da41b19b01fe31039afa
David Godinez <davidgodinez@deephaven.io>
2024-11-19 09:39:34 -0700

    add all options and camel case

--- plugins/ui/src/deephaven/ui/components/toast.py
+++ plugins/ui/src/deephaven/ui/components/toast.py
@@ -2,7 +2,7 @@ from ..hooks import use_event_queue
 
 from typing import Callable
 from .make_component import make_component
-from .._internal.utils import create_props
+from .._internal.utils import dict_to_react_props
 from ..types import ToastVariant
 
 _TOAST_EVENT = "toast.event"
@@ -12,6 +12,11 @@ _TOAST_EVENT = "toast.event"
 def toast(
     message: str,
     variant: ToastVariant = "neutral",
+    action_label: str | None = None,
+    on_action: Callable[[], None] | None = None,
+    on_close: Callable[[], None] | None = None,
+    timeout: int | None = None,
+    id: str | None = None,
 ) -> Callable[[], None]:
     """
     Toasts display brief, temporary notifications of actions, errors, or other events in an application.
@@ -22,4 +27,15 @@ def toast(
     """
     event_queue = use_event_queue()
     # _, params = create_props(locals()) TODO should be able to use locals() here
-    return lambda: event_queue(_TOAST_EVENT, {"message": message, "variant": variant})
+    params = dict_to_react_props(
+        {
+            "message": message,
+            "variant": variant,
+            "action_label": action_label,
+            "on_action": on_action,
+            "on_close": on_close,
+            "timeout": timeout,
+            "id": id,
+        }
+    )
+    return lambda: event_queue(_TOAST_EVENT, params)
