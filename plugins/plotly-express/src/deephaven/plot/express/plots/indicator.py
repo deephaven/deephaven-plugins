ea2d5640386d287ff2935cd9e1599e3667b84735
Joe Numainville <josephnumainville@deephaven.io>
2024-12-13 11:33:00 -0600

    wip

--- plugins/plotly-express/src/deephaven/plot/express/plots/indicator.py
+++ plugins/plotly-express/src/deephaven/plot/express/plots/indicator.py
@@ -11,17 +11,18 @@ from typing import Literal
 Gauge = Literal["shape", "bullet"]
 
 
-def indicator(
+def indicators(
     table: PartitionableTableLike,
     value: str | None = None,
     reference: str | None = None,
-    increasing_color: str | None = None,
-    decreasing_color: str | None = None,
+    by: str | list[str] | None = None,
+    increasing_color: str | list[str] | None = None,
+    decreasing_color: str | list[str] | None = None,
+    text: str | None = None,
     increasing_color_discrete_sequence: list[str] | None = None,
     increasing_color_discrete_map: dict[str | tuple[str], str] | None = None,
     decreasing_color_discrete_sequence: list[str] | None = None,
     decreasing_color_discrete_map: dict[str | tuple[str], str] | None = None,
-    text: str | None = None,
     number: bool = True,
     delta: bool = True,
     gauge: Gauge | None = None,
@@ -31,4 +32,39 @@ def indicator(
     rows: int = 1,
     columns: int = 1,
 ) -> DeephavenFigure:
+    """
+    Create an indicator chart.
+
+    Args:
+      table: A table to pull data from.
+      value: The column to use as the value.
+      reference: The column to use as the reference value.
+      by: A column or list of columns that contain values to plot the figure traces by.
+        All values or combination of values map to a unique design. The variable
+        by_vars specifies which design elements are used.
+        This is overriden if any specialized design variables such as color are specified
+      increasing_color: A column or list of columns that contain color values.
+        The value is used for a plot by on color.
+        see color_discrete_map for additional behaviors.
+      decreasing_color: A column or list of columns that contain color values.
+        The value is used for a plot by on color.
+        see color_discrete_map for additional behaviors
+      text: A column that contains text annotations.
+      increasing_color_discrete_sequence:
+      increasing_color_discrete_map:
+      decreasing_color_discrete_sequence:
+      decreasing_color_discrete_map:
+      number: True to show the number, False to hide it.
+      delta: True to show the delta, False to hide it.
+      gauge: Specifies the type of gauge to use.
+        Set to "angular" for a half-circle gauge and bullet for a horizontal gauge.
+      axis: True to show the axis. Only valid if gauge is set.
+      prefix: A string to prepend to the value.
+      suffix: A string to append to the value.
+      rows: The number of rows of indicators to create.
+      columns: The number of columns of indicators to create.
+
+    Returns:
+
+    """
     raise NotImplementedError
