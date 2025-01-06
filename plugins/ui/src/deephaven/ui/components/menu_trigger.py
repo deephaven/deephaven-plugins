66be7a3a48fb345ca0e74a6cb198f24cc8b228c8
David Godinez <davidgodinez@deephaven.io>
2025-01-06 14:47:06 -0700

    menu trigger docs clean up

--- plugins/ui/src/deephaven/ui/components/menu_trigger.py
+++ plugins/ui/src/deephaven/ui/components/menu_trigger.py
@@ -26,13 +26,14 @@ def menu_trigger(
 ) -> MenuTrigger:
     """
     A menu_trigger serves as a wrapper around a menu and its associated trigger.
+
     Args:
         *children: The menu and its trigger element.
         align: The alignment of the menu relative to the trigger.
         direction: Where the Menu opens relative to its trigger.
         should_flip: Whether the menu should automatically flip direction when space is limited.
         close_on_select: Whether the menu should close when an item is selected.
-        trigger: 	How the menu is triggered.
+        trigger: How the menu is triggered.
         is_open: Whether the menu is open by default (controlled).
         default_open: Whether the menu is open by default (uncontrolled).
         on_open_change: Handler that is called when the overlay's open state changes.
@@ -41,5 +42,6 @@ def menu_trigger(
     Returns:
         The menu trigger element.
     """
+
     children, props = create_props(locals())
     return component_element("MenuTrigger", *children, **props)
