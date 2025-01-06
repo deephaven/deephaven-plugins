8d1ad2b6c7fe26334717973867471f95405d3bd6
David Godinez <davidgodinez@deephaven.io>
2025-01-06 13:18:42 -0700

    code clean up

--- plugins/ui/src/js/src/elements/Menu.tsx
+++ plugins/ui/src/js/src/elements/Menu.tsx
@@ -4,12 +4,10 @@ import {
   SpectrumMenuProps as DHCMenuProps,
 } from '@deephaven/components';
 import { useMenuProps, SerializedMenuProps } from './hooks/useMenuProps';
-// import useConditionalCallback from './hooks/useConditionalCallback';
 
 export function Menu(
   props: SerializedMenuProps<DHCMenuProps<object>>
 ): JSX.Element {
-  console.log('Menu', props);
   const menuProps = useMenuProps(props);
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <DHCMenu {...menuProps} />;
