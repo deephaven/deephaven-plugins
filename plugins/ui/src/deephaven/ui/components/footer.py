9b38e75b1f4fc1319436a2c346332528ac692377
Eric Lin <106491820+ericlln@users.noreply.github.com>
2025-02-03 11:03:40 -0500

    feat: ui.footer (#1100)
    Closes #878

--- plugins/ui/src/deephaven/ui/components/footer.py
+++ plugins/ui/src/deephaven/ui/components/footer.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from typing import Any, Callable
+
+from .basic import component_element
+from ..elements import Element, NodeType
 from .types import (
-    # Layout
     AlignSelf,
     CSSProperties,
     DimensionValue,
@@ -9,12 +10,10 @@ from .types import (
     LayoutFlex,
     Position,
 )
-from .basic import component_element
-from ..elements import Element
 
 
-def content(
-    *children: Any,
+def footer(
+    *children: NodeType,
     flex: LayoutFlex | None = None,
     flex_grow: float | None = None,
     flex_shrink: float | None = None,
@@ -57,56 +56,57 @@ def content(
     key: str | None = None,
 ) -> Element:
     """
-    Content represents the primary content within a Spectrum container.
+    A footer for a document or section.
 
     Args:
-        *children: The content to render within the container.
+        *children: The items to render within the footer.
         flex: When used in a flex layout, specifies how the element will grow or shrink to fit the space available.
-        flex_grow: When used in a flex layout, specifies how the element will grow to fit the space available.
-        flex_shrink: When used in a flex layout, specifies how the element will shrink to fit the space available.
-        flex_basis: When used in a flex layout, specifies the initial main size of the element.
-        align_self: Overrides the alignItems property of a flex or grid container.
-        justify_self: Species how the element is justified inside a flex or grid container.
+        flex_grow: When used in a flex layout, specifies how much the element will grow to fit the space available.
+        flex_shrink: When used in a flex layout, specifies how much the element will shrink to fit the space available.
+        flex_basis: When used in a flex layout, specifies the initial size of the element.
+        align_self: Overrides the align_items property of a flex or grid container.
+        justify_self: Specifies how the element is justified inside a flex or grid container.
         order: The layout order for the element within a flex or grid container.
-        grid_area: When used in a grid layout specifies, specifies the named grid area that the element should be placed in within the grid.
-        grid_row: When used in a grid layout, specifies the row the element should be placed in within the grid.
-        grid_column: When used in a grid layout, specifies the column the element should be placed in within the grid.
-        grid_row_start: When used in a grid layout, specifies the starting row to span within the grid.
-        grid_row_end: When used in a grid layout, specifies the ending row to span within the grid.
-        grid_column_start: When used in a grid layout, specifies the starting column to span within the grid.
-        grid_column_end: When used in a grid layout, specifies the ending column to span within the grid.
-        margin: The margin for all four sides of the element.
-        margin_top: The margin for the top side of the element.
-        margin_bottom: The margin for the bottom side of the element.
-        margin_start: The margin for the logical start side of the element, depending on layout direction.
-        margin_end: The margin for the logical end side of the element, depending on layout direction.
-        margin_x: The margin for the left and right sides of the element.
-        margin_y: The margin for the top and bottom sides of the element.
+        grid_area: The name of the grid area to place the element in.
+        grid_row: The name of the grid row to place the element in.
+        grid_row_start: The name of the grid row to start the element in.
+        grid_row_end: The name of the grid row to end the element in.
+        grid_column: The name of the grid column to place the element in.
+        grid_column_start: The name of the grid column to start the element in.
+        grid_column_end: The name of the grid column to end the element in.
+        margin: The margin to apply around the element.
+        margin_top: The margin to apply above the element.
+        margin_bottom: The margin to apply below the element.
+        margin_start: The margin to apply before the element.
+        margin_end: The margin to apply after the element.
+        margin_x: The margin to apply to the left and right of the element.
+        margin_y: The margin to apply to the top and bottom of the element.
         width: The width of the element.
-        min_width: The minimum width of the element.
-        max_width: The maximum width of the element.
         height: The height of the element.
+        min_width: The minimum width of the element.
         min_height: The minimum height of the element.
+        max_width: The maximum width of the element.
         max_height: The maximum height of the element.
-        position: The position of the element.
+        position: Specifies how the element is positioned.
         top: The distance from the top of the containing element.
         bottom: The distance from the bottom of the containing element.
+        start: The distance from the start of the containing element.
+        end: The distance from the end of the containing element.
         left: The distance from the left of the containing element.
         right: The distance from the right of the containing element.
-        start: The distance from the start of the containing element, depending on layout direction.
-        end: The distance from the end of the containing element, depending on layout direction.
         z_index: The stack order of the element.
         is_hidden: Whether the element is hidden.
-        id: The unique identifier of the element.
+        id: A unique identifier for the element.
         UNSAFE_class_name: A CSS class to apply to the element.
         UNSAFE_style: A CSS style to apply to the element.
         key: A unique identifier used by React to render elements in a list.
 
+
     Returns:
-        The rendered content element.
+        The rendered footer element.
     """
     return component_element(
-        "Content",
+        "Footer",
         children=children,
         flex=flex,
         flex_grow=flex_grow,
@@ -116,12 +116,12 @@ def content(
         justify_self=justify_self,
         order=order,
         grid_area=grid_area,
-        grid_row=grid_row,
-        grid_row_start=grid_row_start,
-        grid_row_end=grid_row_end,
         grid_column=grid_column,
+        grid_row=grid_row,
         grid_column_start=grid_column_start,
         grid_column_end=grid_column_end,
+        grid_row_start=grid_row_start,
+        grid_row_end=grid_row_end,
         margin=margin,
         margin_top=margin_top,
         margin_bottom=margin_bottom,
@@ -130,18 +130,18 @@ def content(
         margin_x=margin_x,
         margin_y=margin_y,
         width=width,
-        height=height,
         min_width=min_width,
-        min_height=min_height,
         max_width=max_width,
+        height=height,
+        min_height=min_height,
         max_height=max_height,
         position=position,
         top=top,
         bottom=bottom,
-        start=start,
-        end=end,
         left=left,
         right=right,
+        start=start,
+        end=end,
         z_index=z_index,
         is_hidden=is_hidden,
         id=id,
