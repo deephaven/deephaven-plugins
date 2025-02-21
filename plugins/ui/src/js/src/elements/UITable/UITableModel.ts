024811d8144f4bbeb5aaa43a5a297d9e788fe27d
Matthew Runyon <matthewrunyon@deephaven.io>
2025-02-21 13:35:33 -0600

    feat: UI table respond to non-primitive prop changes (#1046)
    Fixes #982
    
    Also fixes a bug I found while implementing this where adding a custom
    column in the UI would cause formatting rule `if_` clauses to be
    removed.

--- plugins/ui/src/js/src/elements/UITable/UITableModel.ts
+++ plugins/ui/src/js/src/elements/UITable/UITableModel.ts
@@ -23,48 +23,25 @@ import { type dh as DhType } from '@deephaven/jsapi-types';
 import { ColorGradient, DatabarConfig, FormattingRule } from './UITableUtils';
 import JsTableProxy, { UITableLayoutHints } from './JsTableProxy';
 
+/**
+ * Create a UITableModel.
+ * @param dh The JS API object
+ * @param baseTable The base table to create the UI table model from.
+ * @param databars Databar configurations
+ * @param layoutHints Layout hints for the table
+ * @param format Format rules for the table
+ * @param displayNameMap Column display name mappings
+ * @returns Promise resolving to a new UITableModel
+ */
 export async function makeUiTableModel(
   dh: typeof DhType,
-  table: DhType.Table,
+  baseTableProp: DhType.Table,
   databars: DatabarConfig[],
   layoutHints: UITableLayoutHints,
   format: FormattingRule[],
   displayNameMap: Record<string, string>
 ): Promise<UITableModel> {
-  const joinColumns: string[] = [];
-  const totalsOperationMap: Record<string, string[]> = {};
-  databars.forEach(config => {
-    const { column, value_column: valueColumn = column, min, max } = config;
-
-    try {
-      table.findColumn(column);
-    } catch {
-      throw new Error(`Can't find databar column ${column}`);
-    }
-
-    try {
-      table.findColumn(valueColumn);
-    } catch {
-      throw new Error(`Can't find databar value column ${valueColumn}`);
-    }
-
-    if (min == null && max == null) {
-      totalsOperationMap[valueColumn] = ['Min', 'Max'];
-      joinColumns.push(
-        `${valueColumn}__DATABAR_Min=${valueColumn}__Min`,
-        `${valueColumn}__DATABAR_Max=${valueColumn}__Max`
-      );
-    } else if (min == null) {
-      totalsOperationMap[valueColumn] = ['Min'];
-      joinColumns.push(`${valueColumn}__DATABAR_Min=${valueColumn}`);
-    } else if (max == null) {
-      totalsOperationMap[valueColumn] = ['Max'];
-      joinColumns.push(`${valueColumn}__DATABAR_Max=${valueColumn}`);
-    }
-  });
-
-  let baseTable = table;
-
+  const baseTable = await baseTableProp.copy();
   const customColumns: string[] = [];
   format.forEach((rule, i) => {
     const { if_ } = rule;
@@ -90,21 +67,62 @@ export async function makeUiTableModel(
     });
   }
 
-  if (joinColumns.length > 0) {
-    const totalsTable = await table.getTotalsTable({
+  const joinColumns: string[] = [];
+  const totalsOperationMap: Record<string, string[]> = {};
+  databars.forEach(config => {
+    const { column, value_column: valueColumn = column, min, max } = config;
+
+    try {
+      baseTable.findColumn(column);
+    } catch {
+      throw new Error(`Can't find databar column ${column}`);
+    }
+
+    try {
+      baseTable.findColumn(valueColumn);
+    } catch {
+      throw new Error(`Can't find databar value column ${valueColumn}`);
+    }
+
+    if (min == null && max == null) {
+      totalsOperationMap[valueColumn] = ['Min', 'Max'];
+      joinColumns.push(
+        `${valueColumn}__DATABAR_Min=${valueColumn}__Min`,
+        `${valueColumn}__DATABAR_Max=${valueColumn}__Max`
+      );
+    } else if (min == null) {
+      totalsOperationMap[valueColumn] = ['Min'];
+      joinColumns.push(`${valueColumn}__DATABAR_Min=${valueColumn}`);
+    } else if (max == null) {
+      totalsOperationMap[valueColumn] = ['Max'];
+      joinColumns.push(`${valueColumn}__DATABAR_Max=${valueColumn}`);
+    }
+  });
+
+  let table = baseTable;
+  let totalsTable: DhType.TotalsTable | undefined;
+
+  if (Object.keys(totalsOperationMap).length > 0) {
+    totalsTable = await baseTable.getTotalsTable({
       operationMap: totalsOperationMap,
       defaultOperation: 'Skip',
       showGrandTotalsByDefault: false,
       showTotalsByDefault: false,
       groupBy: [],
     });
-
-    baseTable = await table.naturalJoin(totalsTable, [], joinColumns);
+    table = await baseTable.naturalJoin(totalsTable, [], joinColumns);
   }
 
   const uiTableProxy = new JsTableProxy({
-    table: baseTable,
+    table,
     layoutHints,
+    onClose: () => {
+      // Need to cleanup the base tables when the proxy is closed if we needed aggregations
+      if (totalsTable != null) {
+        baseTable.close();
+        totalsTable.close();
+      }
+    },
   });
 
   const baseModel = await IrisGridModelFactory.makeModel(dh, uiTableProxy);
@@ -112,7 +130,6 @@ export async function makeUiTableModel(
   return new UITableModel({
     dh,
     model: baseModel,
-    table: uiTableProxy,
     databars,
     format,
     displayNameMap,
@@ -144,14 +161,14 @@ type NumericValue =
 class UITableModel extends IrisGridModel {
   private model: IrisGridModel;
 
-  private databars: Map<ColumnName, DatabarConfig>;
-
   /**
    * Map of theme color keys to hex color values
    */
   private colorMap: Map<string, string> = new Map();
 
   private displayNameMap: Record<string, string>;
+
+  private databars: Map<ColumnName, DatabarConfig>;
 
   private format: FormattingRule[];
 
@@ -164,7 +181,6 @@ class UITableModel extends IrisGridModel {
   }: {
     dh: typeof DhType;
     model: IrisGridModel;
-    table: DhType.Table;
     databars: DatabarConfig[];
     format: FormattingRule[];
     displayNameMap: Record<string, string>;
