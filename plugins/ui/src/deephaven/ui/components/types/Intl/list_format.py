8c7b6e2190e9652fad58dd138eb41605c97ba64b
Eric Lin <ericlin@deephaven.io>
2025-03-14 12:08:06 -0400

    fix typing directive for python version < 3.11

--- plugins/ui/src/deephaven/ui/components/types/Intl/list_format.py
+++ plugins/ui/src/deephaven/ui/components/types/Intl/list_format.py
@@ -1,4 +1,11 @@
-from typing import Literal, TypedDict, NotRequired
+from typing import Literal
+
+import sys
+
+if sys.version_info < (3, 11):
+    from typing_extensions import TypedDict, NotRequired
+else:
+    from typing import TypedDict, NotRequired
 
 
 class ListFormatOptions(TypedDict):
