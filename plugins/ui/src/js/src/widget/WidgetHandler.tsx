deephaven/deephaven-plugins

commit 34c0b60fec0aac63d7b7373125363ac6a9094c24

fix: Callable cleanup race (DH-18536) (#1113)
- Client was calling a closeCallable function each time any callable was
no longer rendered
- Callables as part of the regular render cycle that didn't change were
being recreated every time
- Since it wasn't the same object between renders, closeCallable was
getting called for a previous instance of that callable but with the
same ID
- Server side was unnecessarily listening for those to close callables
as part of the regular render cycle
- Things that are no longer rendered are already cleaned up by the
server, don't need to/shouldn't listen to the client for when to clean
those up. Only needed for the temporary callables
- Tested by using the snippet from DH-18536:
```python
from deephaven import ui, time_table

@ui.component
def ui_resetable_table():
    table, set_table = ui.use_state(lambda: time_table("PT1s"))
    handle_press = ui.use_liveness_scope(lambda _: set_table(time_table("PT1s")), [])
    return [
        ui.action_button(
            "Reset",
            on_press=handle_press,
        ),
        table,
    ]

resetable_table = ui_resetable_table()
```
- Also tested using the snippets from previous callable implentation
ticket: https://github.com/deephaven/deephaven-plugins/pull/540
```python
from deephaven import ui

@ui.component
def my_comp():
    on_press = ui.use_callback(lambda d: print(d), [])
    on_press_nested = ui.use_callback(lambda: print, [])
    on_press_double_nested = ui.use_callback(lambda: { "nestedFn": print, "some_val": 4 }, [])
    on_press_unserializable = ui.use_callback(lambda: set([1, 2, 3]), [])
    return [
        ui.button("Normal", on_press=on_press),
        ui.button("Nested", on_press=on_press_nested),
        ui.button("Double Nested", on_press=on_press_double_nested),
        ui.button("Not Serializable", on_press=on_press_unserializable)
    ]

c = my_comp()
```

plugins/ui/src/js/src/widget/WidgetHandler.tsx
@@ -124,6 +124,12 @@ function WidgetHandler({
   );
   const exportedObjectCount = useRef(0);
 
+  // Keep track of callables that are currently rendered/in use
+  // We want to retain the same callable between renders, so we use a map that persists between renders
+  const renderedCallableMap = useRef(
+    new Map<string, (...args: unknown[]) => void>()
+  );
+
   // Bi-directional communication as defined in https://www.npmjs.com/package/json-rpc-2.0
   const jsonClient = useMemo(
     () =>
@@ -178,20 +184,29 @@ function WidgetHandler({
      */
     (data: string) => {
       assertNotNull(jsonClient);
-      // Keep track of exported objects that are no longer in use after this render.
+      // Keep track of exported objects and callables that are no longer in use after this render.
       // We close those objects that are no longer referenced, as they will never be referenced again.
       const deadObjectMap = new Map(exportedObjectMap.current);
+      const deadCallableMap = new Map(renderedCallableMap.current);
 
       const parsedData = JSON.parse(data, (key, value) => {
         // Need to re-hydrate any objects that are defined
         if (isCallableNode(value)) {
           const callableId = value[CALLABLE_KEY];
+          deadCallableMap.delete(callableId);
+          if (renderedCallableMap.current.has(callableId)) {
+            log.debug2('Reusing callableId', callableId);
+            return renderedCallableMap.current.get(callableId);
+          }
           log.debug2('Registering callableId', callableId);
-          return wrapCallable(
+          const callable = wrapCallable(
             jsonClient,
             callableId,
-            callableFinalizationRegistry
+            callableFinalizationRegistry,
+            false
           );
+          renderedCallableMap.current.set(callableId, callable);
+          return callable;
         }
         if (isObjectNode(value)) {
           // Replace this node with the exported object
@@ -223,6 +238,12 @@ function WidgetHandler({
         log.debug('Closing dead object', objectKey);
         deadObject.close();
         exportedObjectMap.current.delete(objectKey);
+      });
+
+      // Close any callables that are no longer referenced
+      deadCallableMap.forEach((deadCallable, callableId) => {
+        log.debug2('Callable no longer rendered:', callableId);
+        renderedCallableMap.current.delete(callableId);
       });
 
       log.debug2(

plugins/ui/src/deephaven/ui/object_types/ElementMessageStream.py
@@ -441,13 +441,12 @@ class ElementMessageStream(MessageStream):
 
     def _close_callable(self, callable_id: str) -> None:
         """
-        Close a callable by its ID.
+        Close a callable by its ID. Used to close a temporary callable that is outside of the render cycle.
 
         Args:
             callable_id: The ID of the callable to close
         """
         logger.debug("Closing callable %s", callable_id)
-        self._callable_dict.pop(callable_id, None)
         self._temp_callable_dict.pop(callable_id, None)
 
     def _send_document_update(

plugins/ui/src/js/src/widget/WidgetUtils.test.tsx
@@ -29,7 +29,7 @@ const mockJsonClient = TestUtils.createMockProxy<JSONRPCServerAndClient>({
   request: mockJsonRequest,
 });
 
-const mockFinilizationRegistry = TestUtils.createMockProxy<
+const mockFinalizationRegistry = TestUtils.createMockProxy<
   FinalizationRegistry<unknown>
 >({
   register: jest.fn(),
@@ -119,7 +119,7 @@ describe('wrapCallable', () => {
   });
 
   it('should return a function that sends a request to the client', () => {
-    wrapCallable(mockJsonClient, 'testMethod', mockFinilizationRegistry)();
+    wrapCallable(mockJsonClient, 'testMethod', mockFinalizationRegistry)();
     expect(mockJsonClient.request).toHaveBeenCalledWith('callCallable', [
       'testMethod',
       [],
@@ -130,7 +130,7 @@ describe('wrapCallable', () => {
     wrapCallable(
       mockJsonClient,
       'testMethod',
-      mockFinilizationRegistry
+      mockFinalizationRegistry
     )('a', { b: 'b' });
     expect(mockJsonClient.request).toHaveBeenCalledWith('callCallable', [
       'testMethod',
@@ -142,14 +142,19 @@ describe('wrapCallable', () => {
     const wrapped = wrapCallable(
       mockJsonClient,
       'testMethod',
-      mockFinilizationRegistry
+      mockFinalizationRegistry
     );
 
-    expect(mockFinilizationRegistry.register).toHaveBeenCalledWith(
+    expect(mockFinalizationRegistry.register).toHaveBeenCalledWith(
       wrapped,
       'testMethod',
       wrapped
     );
+  });
+
+  it('should not register the function in the finalization registry if the shouldRegister parameter is false', () => {
+    wrapCallable(mockJsonClient, 'testMethod', mockFinalizationRegistry, false);
+    expect(mockFinalizationRegistry.register).not.toHaveBeenCalled();
   });
 
   it('should wrap returned callables', async () => {
@@ -162,12 +167,12 @@ describe('wrapCallable', () => {
     const wrappedResult = await wrapCallable(
       mockJsonClient,
       'testMethod',
-      mockFinilizationRegistry
+      mockFinalizationRegistry
     )();
     expect(wrappedResult).toBeInstanceOf(Function);
 
-    expect(mockFinilizationRegistry.register).toHaveBeenCalledTimes(2);
-    expect(mockFinilizationRegistry.register).toHaveBeenLastCalledWith(
+    expect(mockFinalizationRegistry.register).toHaveBeenCalledTimes(2);
+    expect(mockFinalizationRegistry.register).toHaveBeenLastCalledWith(
       wrappedResult,
       'nestedCb',
       wrappedResult
@@ -187,7 +192,7 @@ describe('wrapCallable', () => {
     const wrappedResult = (await wrapCallable(
       mockJsonClient,
       'testMethod',
-      mockFinilizationRegistry
+      mockFinalizationRegistry
     )()) as { nestedCallable: () => void; someOtherProp: string };
 
     expect(wrappedResult).toMatchObject({
@@ -195,8 +200,38 @@ describe('wrapCallable', () => {
       someOtherProp: 'mock',
     });
 
-    expect(mockFinilizationRegistry.register).toHaveBeenCalledTimes(2);
-    expect(mockFinilizationRegistry.register).toHaveBeenLastCalledWith(
+    expect(mockFinalizationRegistry.register).toHaveBeenCalledTimes(2);
+    expect(mockFinalizationRegistry.register).toHaveBeenLastCalledWith(
+      wrappedResult.nestedCallable,
+      'nestedCb',
+      wrappedResult.nestedCallable
+    );
+  });
+
+  it('should wrap nested returned callables even if the parent is not registered', async () => {
+    mockJsonRequest.mockResolvedValueOnce(
+      JSON.stringify({
+        nestedCallable: {
+          [CALLABLE_KEY]: 'nestedCb',
+        },
+        someOtherProp: 'mock',
+      })
+    );
+
+    const wrappedResult = (await wrapCallable(
+      mockJsonClient,
+      'testMethod',
+      mockFinalizationRegistry,
+      false
+    )()) as { nestedCallable: () => void; someOtherProp: string };
+
+    expect(wrappedResult).toMatchObject({
+      nestedCallable: expect.any(Function),
+      someOtherProp: 'mock',
+    });
+
+    expect(mockFinalizationRegistry.register).toHaveBeenCalledTimes(1);
+    expect(mockFinalizationRegistry.register).toHaveBeenCalledWith(
       wrappedResult.nestedCallable,
       'nestedCb',
       wrappedResult.nestedCallable
@@ -207,7 +242,7 @@ describe('wrapCallable', () => {
     mockJsonRequest.mockResolvedValueOnce('not a json string');
 
     expect(
-      wrapCallable(mockJsonClient, 'testMethod', mockFinilizationRegistry)()
+      wrapCallable(mockJsonClient, 'testMethod', mockFinalizationRegistry)()
     ).rejects.toBeInstanceOf(Error);
   });
 });

plugins/ui/src/js/src/widget/WidgetUtils.tsx
@@ -266,12 +266,14 @@ export function getPreservedData(
  * @param jsonClient The JSON client to send callable requests to
  * @param callableId The callableId to return a wrapped callable for
  * @param registry The finalization registry to register the callable with.
+ * @param shouldRegister Whether to register the callable in the finalization registry
  * @returns A wrapped callable that will automatically wrap any nested callables returned by the server
  */
 export function wrapCallable(
   jsonClient: JSONRPCServerAndClient,
   callableId: string,
-  registry: FinalizationRegistry<string>
+  registry: FinalizationRegistry<string>,
+  shouldRegister = true
 ): (...args: unknown[]) => Promise<unknown> {
   const callable = async (...args: unknown[]) => {
     log.debug2(`Callable ${callableId} called`, args);
@@ -304,7 +306,9 @@ export function wrapCallable(
     }
   };
 
-  registry.register(callable, callableId, callable);
+  if (shouldRegister) {
+    registry.register(callable, callableId, callable);
+  }
 
   return callable;
 }

commit 54d62cef129864e9b0751917ceadff8c94dca580

fix: Re-opening widget would break interactivity (DH-18090)
- Issue caused by #1113
- Re-opening the widget would call an old version of the callback
- Reset the rendered callable map when re-initializing the widget

plugins/ui/src/js/src/widget/WidgetHandler.tsx
@@ -399,6 +399,7 @@ function WidgetHandler({
       >();
       exportedObjectMap.current = widgetExportedObjectMap;
       exportedObjectCount.current = 0;
+      renderedCallableMap.current.clear();
 
       // Set a var to the client that we know will not be null in the closure below
       const activeClient = jsonClient;