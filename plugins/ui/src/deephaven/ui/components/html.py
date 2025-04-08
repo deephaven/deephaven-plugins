1e8174841abf92ce41c13774060e5bc70520937f
David Godinez <davidgodinez@deephaven.io>
2025-04-08 06:58:04 -0600

    more comments

--- plugins/ui/src/deephaven/ui/components/html.py
+++ plugins/ui/src/deephaven/ui/components/html.py
@@ -1,8 +1,6 @@
-"""
-Provides a set of functions for creating raw HTML elements.
-
-The components provided in deephaven.ui should be preferred over this module.
-"""
+# Provides a set of functions for creating raw HTML elements.
+#
+# The components provided in deephaven.ui should be preferred over this module.
 from ..elements import BaseElement
 
 
@@ -22,136 +20,476 @@ def html_element(tag: str, *children, **attributes):
 
 
 def div(*children, **attributes):
+    """
+    Creates an HTML <div> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <div> with the specified children and attributes.
+    """
     return html_element("div", *children, **attributes)
 
 
 def span(*children, **attributes):
+    """
+    Creates an HTML <span> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <span> with the specified children and attributes.
+    """
     return html_element("span", *children, **attributes)
 
 
 def h1(*children, **attributes):
+    """
+    Creates an HTML <h1> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <h1> with the specified children and attributes.
+    """
     return html_element("h1", *children, **attributes)
 
 
 def h2(*children, **attributes):
+    """
+    Creates an HTML <h2> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <h2> with the specified children and attributes.
+    """
     return html_element("h2", *children, **attributes)
 
 
 def h3(*children, **attributes):
+    """
+    Creates an HTML <h3> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <h3> with the specified children and attributes.
+    """
     return html_element("h3", *children, **attributes)
 
 
 def h4(*children, **attributes):
+    """
+    Creates an HTML <h4> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <h4> with the specified children and attributes.
+    """
     return html_element("h4", *children, **attributes)
 
 
 def h5(*children, **attributes):
+    """
+    Creates an HTML <h5> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <h5> with the specified children and attributes.
+    """
     return html_element("h5", *children, **attributes)
 
 
 def h6(*children, **attributes):
+    """
+    Creates an HTML <h6> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <h6> with the specified children and attributes.
+    """
     return html_element("h6", *children, **attributes)
 
 
 def p(*children, **attributes):
+    """
+    Creates an HTML <p> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <p> with the specified children and attributes.
+    """
     return html_element("p", *children, **attributes)
 
 
 def a(*children, **attributes):
+    """
+    Creates an HTML <a> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <a> with the specified children and attributes.
+    """
     return html_element("a", *children, **attributes)
 
 
 def ul(*children, **attributes):
+    """
+    Creates an HTML <ul> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <ul> with the specified children and attributes.
+    """
     return html_element("ul", *children, **attributes)
 
 
 def ol(*children, **attributes):
+    """
+    Creates an HTML <ol> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <ol> with the specified children and attributes.
+    """
     return html_element("ol", *children, **attributes)
 
 
 def li(*children, **attributes):
+    """
+    Creates an HTML <li> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <li> with the specified children and attributes.
+    """
     return html_element("li", *children, **attributes)
 
 
 def table(*children, **attributes):
+    """
+    Creates an HTML <table> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <table> with the specified children and attributes.
+    """
     return html_element("table", *children, **attributes)
 
 
 def thead(*children, **attributes):
+    """
+    Creates an HTML <thead> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <thead> with the specified children and attributes.
+    """
     return html_element("thead", *children, **attributes)
 
 
 def tbody(*children, **attributes):
+    """
+    Creates an HTML <tbody> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <tbody> with the specified children and attributes.
+    """
     return html_element("tbody", *children, **attributes)
 
 
 def tr(*children, **attributes):
+    """
+    Creates an HTML <tr> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <tr> with the specified children and attributes.
+    """
     return html_element("tr", *children, **attributes)
 
 
 def th(*children, **attributes):
+    """
+    Creates an HTML <th> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <th> with the specified children and attributes.
+    """
     return html_element("th", *children, **attributes)
 
 
 def td(*children, **attributes):
+    """
+    Creates an HTML <td> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <td> with the specified children and attributes.
+    """
     return html_element("td", *children, **attributes)
 
 
 def b(*children, **attributes):
+    """
+    Creates an HTML <b> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <b> with the specified children and attributes.
+    """
     return html_element("b", *children, **attributes)
 
 
 def i(*children, **attributes):
+    """
+    Creates an HTML <i> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <i> with the specified children and attributes.
+    """
     return html_element("i", *children, **attributes)
 
 
 def br(*children, **attributes):
+    """
+    Creates an HTML <br> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <br> with the specified children and attributes.
+    """
     return html_element("br", *children, **attributes)
 
 
 def hr(*children, **attributes):
+    """
+    Creates an HTML <hr> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <hr> with the specified children and attributes.
+    """
     return html_element("hr", *children, **attributes)
 
 
 def pre(*children, **attributes):
+    """
+    Creates an HTML <pre> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <pre> with the specified children and attributes.
+    """
     return html_element("pre", *children, **attributes)
 
 
 def code(*children, **attributes):
+    """
+    Creates an HTML <code> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <code> with the specified children and attributes.
+    """
     return html_element("code", *children, **attributes)
 
 
 def img(*children, **attributes):
+    """
+    Creates an HTML <img> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <img> with the specified children and attributes.
+    """
     return html_element("img", *children, **attributes)
 
 
 def button(*children, **attributes):
+    """
+    Creates an HTML <button> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <button> with the specified children and attributes.
+    """
     return html_element("button", *children, **attributes)
 
 
 def input(*children, **attributes):
+    """
+    Creates an HTML <input> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <input> with the specified children and attributes.
+    """
     return html_element("input", *children, **attributes)
 
 
 def form(*children, **attributes):
+    """
+    Creates an HTML <form> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <form> with the specified children and attributes.
+    """
     return html_element("form", *children, **attributes)
 
 
 def label(*children, **attributes):
+    """
+    Creates an HTML <label> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <label> with the specified children and attributes.
+    """
     return html_element("label", *children, **attributes)
 
 
 def select(*children, **attributes):
+    """
+    Creates an HTML <select> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <select> with the specified children and attributes.
+    """
     return html_element("select", *children, **attributes)
 
 
 def option(*children, **attributes):
+    """
+    Creates an HTML <option> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <option> with the specified children and attributes.
+    """
     return html_element("option", *children, **attributes)
 
 
 def textarea(*children, **attributes):
+    """
+    Creates an HTML <textarea> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <textarea> with the specified children and attributes.
+    """
     return html_element("textarea", *children, **attributes)
 
 
 def style(*children, **attributes):
+    """
+    Creates an HTML <style> element with the specified children and attributes.
+
+    Args:
+        *children: The children of the element.
+        **attributes: Attributes to set on the element
+
+    Returns:
+        An HTML element representing a <style> with the specified children and attributes.
+    """
     return html_element("style", *children, **attributes)
