57b8da4542cc194092fb53e8dc6dab41c8baf572
David Godinez <davidgodinez@deephaven.io>
2025-04-14 10:36:27 -0600

    update

--- plugins/ui/src/deephaven/ui/components/input_filters.py
+++ plugins/ui/src/deephaven/ui/components/input_filters.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 from typing import Any, Callable
-from .types import FilterChangeEvent
+
+from .types import FilterChangeEventCallable
 from .basic import component_element
 from ..elements import Element
 from .._internal.utils import create_props
 
 
 def input_filters(
-    on_change: FilterChangeEvent | None = None,
-    # on_change: Callable[[str], None] | None = None,
+    on_change: FilterChangeEventCallable | None = None,
+    on_filters: Callable[[list[str]], None] | None = None,
+    columns: list[str] | None = None,
     key: str | None = None,
 ) -> Element:
     """
     This will call on_input_filters_changes when the filters change on the client.
-    We'll have a utility function to apply filters to a table. // TODO
 
     Args:
-        on_input_filters_changes: A callback function that is called when the input filters change.
+        on_change: Called with list of all FilterChangeEvents when the input filters change.
+        on_filters: Called with list of applicable filter strings when the input filters change.
+        columns: The list of columns to filter on.
         key: A unique identifier used by React to render elements in a list.
 
     Returns:
--- plugins/ui/src/js/src/elements/InputFilters.tsx
+++ plugins/ui/src/js/src/elements/InputFilters.tsx
@@ -4,21 +4,37 @@ import { RootState } from '@deephaven/redux';
 import { getInputFiltersForDashboard } from '@deephaven/dashboard-core-plugins';
 import { FilterChangeEvent } from '@deephaven/dashboard-core-plugins/dist/FilterPlugin';
 
-const EMPTY_FUNCTION = () => undefined;
-
 export interface InputFiltersProps {
   onChange?: (event: FilterChangeEvent[]) => void;
+  onFilters?: (filters: string[]) => void;
+  columns?: string[];
 }
 
 export function InputFilters(props: InputFiltersProps): JSX.Element {
-  const { onChange = EMPTY_FUNCTION } = props;
+  const { onChange, onFilters, columns } = props;
   const inputFilters = useSelector(
     (state: RootState) => getInputFiltersForDashboard(state, 'default') // todo: use the correct dashboard id
   );
+
+  // If onChange is provided, call it with all of the input filters
   useEffect(() => {
-    // Handle the input filter change
-    onChange(inputFilters);
+    if (onChange) {
+      onChange(inputFilters);
+    }
   }, [inputFilters, onChange]);
+
+  // If onFilters is provided, call it with the filters for the columns
+  useEffect(() => {
+    if (onFilters) {
+      const inputFiltersForColumns = columns
+        ? inputFilters.filter(filter => columns.includes(filter.name))
+        : inputFilters;
+      const filters = inputFiltersForColumns
+        .sort((a, b) => a.timestamp - b.timestamp)
+        .map(filter => `${filter.name}=\`${filter.value}\``); // TODO use some util to do this?
+      onFilters(filters);
+    }
+  }, [inputFilters, onFilters, columns]);
 
   return <div>{JSON.stringify(inputFilters)}</div>;
 }
