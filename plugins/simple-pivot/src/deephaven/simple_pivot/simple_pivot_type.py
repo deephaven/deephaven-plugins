6d23ceeb1c297ec61d0d4fd683969cbf3d786a83
Vlad Babich <vladbabich@deephaven.io>
2025-04-29 11:11:07 -0600

    Change SimplePivotType name

--- plugins/simple-pivot/src/deephaven/simple_pivot/simple_pivot_type.py
+++ plugins/simple-pivot/src/deephaven/simple_pivot/simple_pivot_type.py
@@ -79,7 +79,7 @@ class SimplePivotType(BidirectionalObjectType):
             str: The name of the object
         """
         # this name should match the supportedTypes in SimplePivotPlugin.ts
-        return "simplepivot.SimplePivotTable"
+        return "test.simplepivot.SimplePivotTable"
 
     def is_type(self, obj: Any) -> bool:
         """
