80c8cee1d57a697f6a524f49b3264fff59237469
Vlad Babich <vladbabich@deephaven.io>
2025-04-29 09:50:37 -0600

    Fix setup.py

--- plugins/simple-pivot/setup.py
+++ plugins/simple-pivot/setup.py
@@ -5,8 +5,8 @@ from deephaven.plugin.packaging import package_js
 # js_dir is the directory where the JavaScript source files are located
 js_dir = "src/js/"
 # dest_dir is the directory where the JavaScript source files will be copied to in the package
-dest_dir = os.path.join("src/simple_pivot/_js")
+dest_dir = os.path.join("src/deephaven/simple_pivot/_js")
 
 package_js(js_dir, dest_dir)
 
-setup(package_data={"simple_pivot._js": ["**"]})
+setup(package_data={"deephaven.simple_pivot._js": ["**"]})
