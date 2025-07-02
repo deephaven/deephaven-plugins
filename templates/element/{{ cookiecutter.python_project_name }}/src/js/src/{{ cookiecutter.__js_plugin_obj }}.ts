c35a46235dfcc2076aad215b37df970896facc4f
Joe Numainville <josephnumainville@deephaven.io>
2025-07-02 16:03:50 -0500

    wip

--- templates/element/{{ cookiecutter.python_project_name }}/src/js/src/{{ cookiecutter.__js_plugin_obj }}.ts
+++ templates/element/{{ cookiecutter.python_project_name }}/src/js/src/{{ cookiecutter.__js_plugin_obj }}.ts
@@ -1,6 +1,6 @@
 import { type WidgetPlugin, PluginType } from '@deephaven/plugin';
 import { vsGraph } from '@deephaven/icons';
-import { {{ cookiecutter.__js_plugin_view_obj }} } from './{{ cookiecutter.__js_plugin_view_obj }}';
+import {{ cookiecutter.__js_plugin_view_obj }} from './{{ cookiecutter.__js_plugin_view_obj }}';
 
 // Register the plugin with Deephaven
 export const {{ cookiecutter.__js_plugin_obj }}: ElementPlugin = {
@@ -10,7 +10,7 @@ export const {{ cookiecutter.__js_plugin_obj }}: ElementPlugin = {
   type: PluginType.ELEMENT_PLUGIN,
   // The mapping of names to React elements for the plugin. This should match the value returned by `name`
   // in {{ cookiecutter.__type_name }} in {{ cookiecutter.__type_file_name }}.py
-  mapping: {'{{ cookiecutter.__element_name }}': null}
+  mapping: {'{{ cookiecutter.__element_name }}': {{ cookiecutter.__js_plugin_view_obj }} }
 };
 
 export default {{ cookiecutter.__js_plugin_obj }};
