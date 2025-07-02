e9dfa9b173b39e64898449fc82f708be56b90da8
Mike Bender <mikebender@deephaven.io>
2025-07-02 10:18:47 -0400

    feat(ag-grid): Allow grouping to be applied to regular tables (#1186)
    - Add the columns tool panel
    - Allow groupings to be set on a table, and then create a tree table
    from that
    - Supports groupings, aggregations, filtering, sorting
    - Uses the Viewport Row Model

--- plugins/ag-grid/src/js/src/renderers/TreeCellRenderer.tsx
+++ plugins/ag-grid/src/js/src/renderers/TreeCellRenderer.tsx
@@ -2,28 +2,33 @@ import React, { useCallback } from 'react';
 import { CustomCellRendererProps } from '@ag-grid-community/react';
 import { Button } from '@deephaven/components';
 import { vsTriangleDown, vsTriangleRight } from '@deephaven/icons';
-import TreeViewportDatasource, {
-  TREE_NODE_KEY,
-  TreeNode,
-} from './datasources/TreeViewportDatasource';
+import { TREE_NODE_KEY, TreeNode } from '../utils/AgGridTableUtils';
+import DeephavenViewportDatasource from '../datasources/DeephavenViewportDatasource';
 
 export type TreeCellRendererProps = CustomCellRendererProps & {
-  datasource: TreeViewportDatasource;
+  datasource: DeephavenViewportDatasource;
 };
 
 export default function TreeCellRenderer(
   props: TreeCellRendererProps
 ): JSX.Element {
-  const { node, value, datasource } = props;
+  const { node, datasource, api } = props;
   const { data } = node;
   const treeNode: TreeNode | undefined = data?.[TREE_NODE_KEY];
   const { hasChildren = false, depth = 0, isExpanded = false } = treeNode ?? {};
 
   const handleClick = useCallback(() => {
-    if (treeNode != null) {
+    if (treeNode != null && datasource instanceof DeephavenViewportDatasource) {
       datasource.setExpanded(treeNode.index, !treeNode.isExpanded);
     }
   }, [datasource, treeNode]);
+
+  const rowGroupColumns = api.getRowGroupColumns();
+  // If we're on a leaf row, show the last row group column as the group name instead of an empty string.
+  const colDef =
+    rowGroupColumns[hasChildren ? depth - 2 : rowGroupColumns.length - 1];
+  const colId = colDef?.getId();
+  const groupName = data?.[colId];
 
   return (
     <>
@@ -44,10 +49,11 @@ export default function TreeCellRenderer(
             textAlign: 'left',
             justifyContent: 'left',
           }}
-        />
+        >
+          {groupName}
+        </Button>
       )}
-      &nbsp;
-      {value}
+      {!hasChildren && groupName}
     </>
   );
 }
