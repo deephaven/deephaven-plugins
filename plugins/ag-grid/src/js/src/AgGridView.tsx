2ed2d7fb65c9f76f9fa1e46908e37eda64b0ddcb
Germain Zhang-Houle <86877307+gzh2003@users.noreply.github.com>
2025-07-07 13:59:14 -0400

    fix: DH-19685: Auto-size AG Grid to fit cell content (#1205)
    For DH-19685. Implements column and row auto-sizing for AG Grid tables.
    When the grid is initialized and data is loaded in, columns are
    auto-sized to fit their largest cell content, while row heights are set
    based on theme parameters

--- plugins/ag-grid/src/js/src/AgGridView.tsx
+++ plugins/ag-grid/src/js/src/AgGridView.tsx
@@ -3,13 +3,19 @@ import type { dh as DhType } from '@deephaven/jsapi-types';
 import Log from '@deephaven/log';
 import { WorkspaceSettings } from '@deephaven/redux';
 import { createFormatterFromSettings } from '@deephaven/jsapi-utils';
-import { ColDef, GridReadyEvent } from '@ag-grid-community/core';
+import {
+  ColDef,
+  GridReadyEvent,
+  GridApi,
+  GridSizeChangedEvent,
+  FirstDataRenderedEvent,
+} from '@ag-grid-community/core';
 import {
   AgGridReact,
   AgGridReactProps,
   CustomCellRendererProps,
 } from '@ag-grid-community/react';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { getColumnDefs } from './utils/AgGridTableUtils';
 import AgGridFormatter from './utils/AgGridFormatter';
 import TreeCellRenderer from './renderers/TreeCellRenderer';
@@ -33,6 +39,12 @@ export function AgGridView({
   agGridProps,
 }: AgGridViewProps): JSX.Element | null {
   const dh = useApi();
+
+  const gridApiRef = useRef<GridApi | null>(null);
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [isFirstDataRendered, setIsFirstDataRendered] = useState(false);
+  const [isColumnsSized, setIsColumnsSized] = useState(false);
 
   log.debug('AgGridView rendering', table, table?.columns);
 
@@ -74,14 +86,6 @@ export function AgGridView({
     [treeCellRenderer]
   );
 
-  const handleGridReady = useCallback(
-    (event: GridReadyEvent) => {
-      log.debug('handleGridReady', event);
-      datasource.setGridApi(event.api);
-    },
-    [datasource]
-  );
-
   const sideBar = useMemo(
     () => ({
       toolPanels: [
@@ -97,11 +101,51 @@ export function AgGridView({
     []
   );
 
+  // Workaround to auto-size columns based on their contents, as ag-grid ignores virtual columns
+  // that are not visible in the viewport
+  const autoSizeAllColumns = () => {
+    const gridApi = gridApiRef.current;
+    if (!gridApi) return;
+    gridApi.sizeColumnsToFit();
+    const columns = gridApi.getColumns();
+    if (!columns) return;
+    const allColumnIds = columns.map(col => col.getColId());
+    gridApi.autoSizeColumns(allColumnIds);
+  };
+
+  const handleGridReady = useCallback(
+    (event: GridReadyEvent) => {
+      log.debug('handleGridReady', event);
+      datasource.setGridApi(event.api);
+      gridApiRef.current = event.api;
+    },
+    [datasource]
+  );
+
+  const handleFirstDataRendered = (event: FirstDataRenderedEvent) => {
+    log.debug('handleFirstDataRendered', event);
+    setIsFirstDataRendered(true);
+  };
+
+  const handleGridSizeChanged = (event: GridSizeChangedEvent) => {
+    log.debug('handleGridSizeChanged', event);
+    setIsVisible(event.clientHeight > 0 && event.clientWidth > 0);
+  };
+
+  useEffect(() => {
+    if (isVisible && isFirstDataRendered && !isColumnsSized) {
+      setIsColumnsSized(true);
+      autoSizeAllColumns();
+    }
+  }, [isVisible, isFirstDataRendered, isColumnsSized]);
+
   return (
     <AgGridReact
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...agGridProps}
       onGridReady={handleGridReady}
+      onFirstDataRendered={handleFirstDataRendered}
+      onGridSizeChanged={handleGridSizeChanged}
       autoGroupColumnDef={autoGroupColumnDef}
       columnDefs={colDefs}
       dataTypeDefinitions={formatter.cellDataTypeDefinitions}
--- plugins/ag-grid/src/js/src/datasources/DeephavenViewportDatasource.ts
+++ plugins/ag-grid/src/js/src/datasources/DeephavenViewportDatasource.ts
@@ -235,8 +235,8 @@ export class DeephavenViewportDatasource implements IViewportDatasource {
     if (this.currentViewport == null) {
       log.debug('Setting default viewport');
       this.currentViewport = {
-        firstRow: 0,
-        lastRow: 100, // Default to the first 100 rows
+        firstRow: this.gridApi.getFirstDisplayedRowIndex(),
+        lastRow: this.gridApi.getLastDisplayedRowIndex(),
       };
     }
     const { firstRow, lastRow } = this.currentViewport;
