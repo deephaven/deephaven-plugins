423ad84e871c5ff0342266523b27b713f5105b3b
mikebender <mikebender@deephaven.io>
2025-07-23 10:12:03 -0400

    WIP define the key instead of using import.meta.env

--- plugins/ag-grid/src/js/vite.config.ts
+++ plugins/ag-grid/src/js/vite.config.ts
@@ -1,32 +1,43 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  build: {
-    minify: false,
-    outDir: 'dist/bundle',
-    lib: {
-      entry: './src/index.ts',
-      fileName: () => 'index.js',
-      formats: ['cjs'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const isProduction = mode === 'production';
+  return {
+    build: {
+      minify: false,
+      outDir: 'dist/bundle',
+      lib: {
+        entry: './src/index.ts',
+        fileName: () => 'index.js',
+        formats: ['cjs'],
+      },
+      rollupOptions: {
+        external: [
+          'react',
+          'react-dom',
+          'redux',
+          'react-redux',
+          '@deephaven/components',
+          '@deephaven/icons',
+          '@deephaven/jsapi-bootstrap',
+          '@deephaven/log',
+          '@deephaven/plugin',
+        ],
+      },
     },
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'redux',
-        'react-redux',
-        '@deephaven/components',
-        '@deephaven/icons',
-        '@deephaven/jsapi-bootstrap',
-        '@deephaven/log',
-        '@deephaven/plugin',
-      ],
-    },
-  },
-  define:
-    mode === 'production' ? { 'process.env.NODE_ENV': '"production"' } : {},
-  plugins: [react()],
-}));
+    define: isProduction
+      ? {
+          'process.env.NODE_ENV': '"production"',
+          // Define this for production so it's baked into the build
+          'import.meta.env.VITE_AG_GRID_LICENSE_KEY': JSON.stringify(
+            env.VITE_AG_GRID_LICENSE_KEY ?? ''
+          ),
+        }
+      : {},
+    plugins: [react()],
+  };
+});
