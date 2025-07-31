006faa6e8cef2bc81684afb14d5496720851a109
gzh2003 <germainzhanghoule@deephaven.io>
2025-07-31 16:37:53 -0400

    Add handling for user-defined alignments

--- plugins/ui/src/js/src/elements/UITable/UITableModel.ts
+++ plugins/ui/src/js/src/elements/UITable/UITableModel.ts
@@ -548,7 +548,19 @@ class UITableModel extends IrisGridModel {
     column: ModelIndex,
     row: ModelIndex
   ): CanvasTextAlign {
+    // Check if the base model has a custom alignment. If so, don't override with UI table formatting.
+    // Text alignment priority hierarchy (from highest to lowest):
+    // 1. Alignment from context menu
+    // 2. UI table formatting
+    // 3. Data type based alignment
+    const customAlignment = isIrisGridTableModelTemplate(this.model)
+      ? this.model.columnAlignmentMap.get(
+          this.model.sourceColumn(column, row).name
+        )
+      : undefined;
+
     return (
+      customAlignment ??
       this.getFormatOptionForCell(column, row, 'alignment') ??
       this.model.textAlignForCell(column, row)
     );
