9bd8d5672bbee39abe9bb3008938b06e8af856fb
Joe Numainville <josephnumainville@deephaven.io>
2025-08-04 11:26:10 -0500

    fixing versions

--- templates/element/{{ cookiecutter.python_project_name }}/src/js/src/{{ cookiecutter.__js_plugin_obj }}.ts
+++ templates/element/{{ cookiecutter.python_project_name }}/src/js/src/{{ cookiecutter.__js_plugin_obj }}.ts
@@ -9,7 +9,10 @@ export const {{ cookiecutter.__js_plugin_obj }}: ElementPlugin = {
   type: PluginType.ELEMENT_PLUGIN,
   // The mapping of names to React elements for the plugin. This should match the value returned by `name`
   // in {{ cookiecutter.__component_name }} in {{ cookiecutter.__component_name }}.py
-  mapping: {'{{ cookiecutter.__element_name }}': {{ cookiecutter.__js_plugin_view_obj }} }
+  mapping: {
+      '{{ cookiecutter.__element_name }}':
+      {{ cookiecutter.__js_plugin_view_obj }}
+  },
 };
 
 export default {{ cookiecutter.__js_plugin_obj }};
