5632b15f39c0c8317c6004ba6db9b8f5237e7a38
mikebender <mikebender@deephaven.io>
2025-09-09 12:54:23 -0400

    fix(ag-grid): Remove dependency on @deephaven/components package
    - We only used it for the LoadingSpinner now.
    - Just bring the LoadingSpinner in

--- plugins/ag-grid/src/js/src/AgGridWidget.tsx
+++ plugins/ag-grid/src/js/src/AgGridWidget.tsx
@@ -1,5 +1,4 @@
 import { useMemo } from 'react';
-import { LoadingOverlay } from '@deephaven/components';
 import type { dh as DhType } from '@deephaven/jsapi-types';
 import { type WidgetComponentProps } from '@deephaven/plugin';
 import { useApi } from '@deephaven/jsapi-bootstrap';
@@ -11,6 +10,8 @@ import AgGridView from './AgGridView';
 import AgGridDhTheme from './AgGridDhTheme';
 import { getDefaultProps } from './utils';
 import useWidgetFetch from './hooks/useWidgetFetch';
+import classNames from 'classnames';
+import LoadingOverlay from './components/LoadingOverlay';
 
 /**
  * Fetches an AgGrid widget from the server and fetches the underlying table provided by the widget.
