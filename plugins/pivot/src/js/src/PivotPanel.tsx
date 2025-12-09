5c73a13023f321d53d2cdcd94aaaf4df819fc378
Vlad Babich <vladbabich@deephaven.io>
2025-12-08 17:08:28 -0700

    Fix eslint

--- plugins/pivot/src/js/src/PivotPanel.tsx
+++ plugins/pivot/src/js/src/PivotPanel.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import { type dh } from '@deephaven/jsapi-types';
 import { LoadingOverlay } from '@deephaven/components';
 import {
@@ -6,11 +7,12 @@ import {
 } from '@deephaven/dashboard-core-plugins';
 import { getErrorMessage } from '@deephaven/utils';
 import type { DashboardPanelProps } from '@deephaven/dashboard';
-import { type Ref, type ForwardRefExoticComponent, forwardRef } from 'react';
-import useHydratePivotGrid from './useHydratePivotGrid';
 import type { WidgetPanelProps } from '@deephaven/plugin';
+import useHydratePivotGrid from './useHydratePivotGrid';
 
 export const PivotPanel = forwardRef(
+  // Unconnected IrisGridPanel type is not exported from dashboard-core-plugins
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (panelProps: WidgetPanelProps<dh.Widget>, ref: React.Ref<any>) => {
     const { localDashboardId, metadata, panelState, ...props } =
       panelProps as DashboardPanelProps & {
