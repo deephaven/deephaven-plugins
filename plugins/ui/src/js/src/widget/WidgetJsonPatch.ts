ffc69996c22ff5d494acfd2e9d2c3ab1048589b8
davidgodinez <davidgodinez@deephaven.io>
2026-02-20 08:22:32 -0700

    clean up

--- plugins/ui/src/js/src/widget/WidgetJsonPatch.ts
+++ plugins/ui/src/js/src/widget/WidgetJsonPatch.ts
@@ -1,7 +1,15 @@
-import { Operation } from 'fast-json-patch';
+import { CopyOperation } from '@deephaven/iris-grid/dist/IrisGridCopyHandler';
+import {
+  AddOperation,
+  MoveOperation,
+  Operation,
+  RemoveOperation,
+  ReplaceOperation,
+  TestOperation,
+} from 'fast-json-patch';
 
 /**
- * Applies a JSON patch to a document. Performs a shallow copy on all objects along the path of the patch. Leaves other parts of the document unchanged.
+ * Applies a JSON patch to a document. Performs a shallow copy on all objects and arrays along the path of the patch. Leaves other parts of the document unchanged.
  *
  * @param document The original document to apply the JSON patch to.
  * @param patch The JSON patch operations to apply.
@@ -12,6 +20,12 @@ export function applyJsonPatch(document: object, patch: Operation[]): object {
   return document;
 }
 
+/**
+ * Performs a shallow copy on all objects and arrays along the specified path in the document.
+ *
+ * @param document The document to perform the shallow copy on.
+ * @param path The JSON pointer path to the property to copy.
+ */
 function shallowCopyPath(document: object, path: string): void {
   // TODO implement
 }
@@ -20,45 +34,44 @@ export function applyOperation(document: object, operation: Operation): void {
   // TODO implement
 }
 
-export function applyAddOperation(
+export function applyAddOperation<T>(
   document: object,
-  operation: Operation
+  operation: AddOperation<T>
 ): void {
   // TODO implement
 }
 
 export function applyRemoveOperation(
   document: object,
-  operation: Operation
+  operation: RemoveOperation
 ): void {
   // TODO implement
 }
 
-export function applyReplaceOperation(
+export function applyReplaceOperation<T>(
   document: object,
-  operation: Operation
+  operation: ReplaceOperation<T>
 ): void {
   // TODO implement
 }
 
 export function applyCopyOperation(
   document: object,
-  operation: Operation
+  operation: CopyOperation
 ): void {
   // TODO implement
 }
 
 export function applyMoveOperation(
   document: object,
-  operation: Operation
+  operation: MoveOperation
 ): void {
   // TODO implement
 }
 
-export function applyTestOperation(
+export function applyTestOperation<T>(
   document: object,
-  operation: Operation
-): boolean {
+  operation: TestOperation<T>
+): void {
   // TODO implement
-  return false;
 }
