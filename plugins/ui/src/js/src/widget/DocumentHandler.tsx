b8fd9ed3a683f7516c7cc6b7074701e0f8ba4bfb
Mike Bender <mikebender@deephaven.io>
2026-03-06 15:18:55 -0500

    feat(ui): DH-19818: add nested dashboard support (#1302)
    - Dashboard component now detects when inside a panel and delegates to
    `NestedDashboard`
    - `NestedDashboard` uses `Dashboard` from `@deephaven/dashboard` to
    create isolated layout
    - `NestedDashboardContent` provides context providers for nested panels
    - `DashboardContent` extracted for top-level dashboard rendering
    - Add E2E tests and Python test fixtures for nested dashboards
    - Add unit tests for `Dashboard` and `NestedDashboard` components
    - Updated documentation with some examples for nested dashboards,
    removed restrictions about nesting dashboards in panels
    
    ---------
    
    Co-authored-by: margaretkennedy <82049573+margaretkennedy@users.noreply.github.com>

--- plugins/ui/src/js/src/widget/DocumentHandler.tsx
+++ plugins/ui/src/js/src/widget/DocumentHandler.tsx
@@ -1,25 +1,12 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
-import { nanoid } from 'nanoid';
+import React from 'react';
 import { WidgetDescriptor } from '@deephaven/dashboard';
 import Log from '@deephaven/log';
-import { EMPTY_ARRAY, EMPTY_FUNCTION } from '@deephaven/utils';
 import { ReactPanelManagerContext } from '../layout/ReactPanelManager';
+import { usePanelManager } from '../layout/usePanelManager';
 import { getRootChildren } from './DocumentUtils';
-import {
-  ReadonlyWidgetData,
-  WidgetData,
-  WidgetDataUpdate,
-} from './WidgetTypes';
+import { ReadonlyWidgetData, WidgetDataUpdate } from './WidgetTypes';
 
 const log = Log.module('@deephaven/js-plugin-ui/DocumentHandler');
-
-const EMPTY_OBJECT = Object.freeze({});
 
 export type DocumentHandlerProps = React.PropsWithChildren<{
   /** Definition of the widget used to create this document. Used for titling panels if necessary. */
@@ -47,133 +34,18 @@ export type DocumentHandlerProps = React.PropsWithChildren<{
 function DocumentHandler({
   children,
   widget,
-  initialData: data = EMPTY_OBJECT,
-  onDataChange = EMPTY_FUNCTION,
+  initialData,
+  onDataChange,
   onClose,
 }: DocumentHandlerProps): JSX.Element {
   log.debug('Rendering document', widget);
-  const panelIdIndex = useRef(0);
 
-  // Using `useState` here to initialize the data only once.
-  // We don't want to use `useMemo`, because we only want it to be initialized once with the `initialData` (uncontrolled)
-  // We don't want to use `useRef`, because we only want to run `structuredClone` once, and you can't pass an
-  // initialization function into `useRef` like you can with `useState`
-  const [widgetData] = useState<WidgetData>(() => structuredClone(data));
-
-  // panelIds that are currently opened within this document. This list is tracked by the `onOpen`/`onClose` call on the `ReactPanelManager` from a child component.
-  // Note that the initial widget data provided will be the `panelIds` for this document to use; this array is what is actually opened currently.
-  const panelIds = useRef<string[]>([]);
-
-  // Flag to signal the panel counts have changed in the last render
-  // We may need to check if we need to close this widget if all panels are closed
-  const [isPanelsDirty, setPanelsDirty] = useState(false);
-
-  const handleOpen = useCallback(
-    (panelId: string) => {
-      if (panelIds.current.includes(panelId)) {
-        throw new Error('Duplicate panel opens received');
-      }
-
-      panelIds.current.push(panelId);
-      log.debug('Panel opened, open count', panelIds.current.length);
-
-      setPanelsDirty(true);
-    },
-    [panelIds]
-  );
-
-  const handleClose = useCallback(
-    (panelId: string) => {
-      const panelIndex = panelIds.current.indexOf(panelId);
-      if (panelIndex === -1) {
-        throw new Error('Panel close received for unknown panel');
-      }
-
-      panelIds.current.splice(panelIndex, 1);
-      log.debug('Panel closed, open count', panelIds.current.length);
-
-      setPanelsDirty(true);
-    },
-    [panelIds]
-  );
-
-  const handleDataChange = useCallback(
-    (panelId: string, panelData: unknown[]) => {
-      onDataChange({
-        panelStates: {
-          ...widgetData.panelStates,
-          [panelId]: panelData,
-        },
-      });
-    },
-    [onDataChange, widgetData]
-  );
-
-  /**
-   * When there are changes made to panels in a render cycle, check if they've all been closed and fire an `onClose` event if they are.
-   * Otherwise, fire an `onDataChange` event with the updated panelIds that are open.
-   */
-  useEffect(
-    function syncOpenPanels() {
-      if (!isPanelsDirty) {
-        return;
-      }
-
-      setPanelsDirty(false);
-
-      // Check if all the panels in this widget are closed
-      // We do it outside of the `handleClose` function in case a new panel opens up in the same render cycle
-      log.debug2(
-        'Widget',
-        widget.id,
-        'open panel count',
-        panelIds.current.length
-      );
-      if (panelIds.current.length === 0) {
-        log.debug('Widget', widget.id, 'closed all panels, triggering onClose');
-        onClose?.();
-      } else {
-        onDataChange({ ...widgetData, panelIds: panelIds.current });
-      }
-    },
-    [isPanelsDirty, widget.id, onClose, onDataChange, widgetData]
-  );
-
-  const getPanelId = useCallback(() => {
-    // On rehydration, yield known IDs first
-    // If there are no more known IDs, generate a new one.
-    // This can happen if the document hasn't been opened before, or if it's rehydrated and a new panel is added.
-    // Note that if the order of panels changes, the worst case scenario is that panels appear in the wrong location in the layout.
-    const panelId = widgetData.panelIds?.[panelIdIndex.current] ?? nanoid();
-    panelIdIndex.current += 1;
-    return panelId;
-  }, [widgetData]);
-
-  const getInitialData = useCallback(
-    (panelId: string) =>
-      widgetData.panelStates?.[panelId] ??
-      (EMPTY_ARRAY as unknown as unknown[]),
-    [widgetData]
-  );
-
-  const panelManager = useMemo(
-    () => ({
-      metadata: widget,
-      onOpen: handleOpen,
-      onClose: handleClose,
-      onDataChange: handleDataChange,
-      getPanelId,
-      getInitialData,
-    }),
-    [
-      widget,
-      getPanelId,
-      handleClose,
-      handleOpen,
-      handleDataChange,
-      getInitialData,
-    ]
-  );
+  const panelManager = usePanelManager({
+    widget,
+    initialData,
+    onDataChange,
+    onClose,
+  });
 
   return (
     <ReactPanelManagerContext.Provider value={panelManager}>
